ad7a7aeaa356757f4cbfcd959d04871833803cf1
Spencer Oliver <spen@spen-soft.co.uk>
2012-02-01 13:08:31 +0000

    checkpatch: increase line length to 120
    Change-Id: I963385d0a4880f2b1e55208c8dfe65c1870ac6e1
    Signed-off-by: Spencer Oliver <spen@spen-soft.co.uk>

--- tools/scripts/checkpatch.pl
+++ tools/scripts/checkpatch.pl
@@ -1664,12 +1664,12 @@ sub process {
 # check we are in a valid source file if not then ignore this hunk
 		next if ($realfile !~ /\.(h|c|s|S|pl|sh)$/);
 
-#100 column limit
+#120 column limit
 		if ($line =~ /^\+/ && $prevrawline !~ /\/\*\*/ &&
 		    $rawline !~ /^.\s*\*\s*\@$Ident\s/ &&
 		    !($line =~ /^\+\s*$logFunctions\s*\(\s*(?:(KERN_\S+\s*|[^"]*))?"[X\t]*"\s*(?:|,|\)\s*;)\s*$/ ||
 		    $line =~ /^\+\s*"[^"]*"\s*(?:\s*|,|\)\s*;)\s*$/) &&
-		    $length > 100)
+		    $length > 120)
 		{
 			WARN("LONG_LINE",
 			     "line over 100 characters\n" . $herecurr);
