particle-iot/openocd

commit 00c864835149a96b431fc8f31dd89542d88fd383

at91sam3: fix warnings
- reduce scope of variables
- assert on post condition to fix clang warning

Change-Id: Id91038f73a632f9688ba52c9c34eae923626e770
Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>
Reviewed-on: http://openocd.zylin.com/160
Tested-by: jenkins
Reviewed-by: Spencer Oliver <spen@spen-soft.co.uk>

src/flash/nor/at91sam3.c
@@ -2926,6 +2926,10 @@ sam3_write(struct flash_bank *bank,
 		page_cur++;
 	}
 
+	/* By checking that offset is correct here, we also
+	fix a clang warning */
+	assert(offset == pPrivate->page_size);
+
 	// intermediate large pages
 	// also - the final *terminal*
 	// if that terminal page is a full page
@@ -2972,14 +2976,13 @@ sam3_write(struct flash_bank *bank,
 COMMAND_HANDLER(sam3_handle_info_command)
 {
 	struct sam3_chip *pChip;
-	unsigned x;
-	int r;
-
 	pChip = get_current_sam3(CMD_CTX);
 	if (!pChip) {
 		return ERROR_OK;
 	}
 
+	unsigned x;
+	int r;
 	r = 0;
 
 	// bank0 must exist before we can do anything

commit 5651a3c96a30096556fe85d317c7d32e69f19722

Fix assert to check flash programming offset
The assert introduced in 00c864835149a96b431fc8f31dd89542d88fd383 checks
whether the programming offset equals to page_size of the flash, while it
wants to check whether the offset is a multiple of the page_size.

Change-Id: I794d021951a28c1cc520b5eea5d500f097721b06
Signed-off-by: Attila Kinali <attila@kinali.ch>

src/flash/nor/at91sam3.c
@@ -2845,7 +2845,7 @@ static int sam3_write(struct flash_bank *bank,
 
 	/* By checking that offset is correct here, we also
 	fix a clang warning */
-	assert(offset == pPrivate->page_size);
+	assert(offset % pPrivate->page_size == 0);
 
 	/* intermediate large pages */
 	/* also - the final *terminal* */