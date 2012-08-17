particle-iot/openocd

commit c3074f377c1da33ca8ba8493826e1b52351eebc6

target: fix segfault in arm7_9 8/16bit read
Seems I5347352e7595686634bd0de13fcf6de6e55027b0 introduced an issue when
reading 8/16 bit data - the in buffer was always set to 32bits.

Change-Id: Ife2bb6a20fcb3ec0e486655512164f25ae9196b4
Signed-off-by: Spencer Oliver <spen@spen-soft.co.uk>
Reviewed-on: http://openocd.zylin.com/660
Tested-by: jenkins
Reviewed-by: Mathias Küster <kesmtp@freenet.de>

src/target/arm7tdmi.c
@@ -223,7 +223,7 @@ static int arm7tdmi_clock_data_in_endianness(struct arm_jtag *jtag_info,
 	fields[0].out_value = NULL;
 	fields[0].in_value = NULL;
 
-	fields[1].num_bits = 32;
+	fields[1].num_bits = size * 8;
 	fields[1].out_value = NULL;
 	fields[1].in_value = in;
 

src/target/arm9tdmi.c
@@ -272,7 +272,7 @@ int arm9tdmi_clock_data_in_endianness(struct arm_jtag *jtag_info,
 	if (retval != ERROR_OK)
 		return retval;
 
-	fields[0].num_bits = 32;
+	fields[0].num_bits = size * 8;
 	fields[0].out_value = NULL;
 	fields[0].in_value = in;
 

commit 3bacaae3ed6c967b59302ae1db2d1671f3407d71

target fix arm7_9 8/16bit read regression
Seems c3074f377c1da33ca8ba8493826e1b52351eebc6 caused a regression
that has shown up in the cfi driver.

Switch back to using 32bit drscan into a temp variable.

Change-Id: I4105bcc8bdd358c904f34e18b9aa14be27e69372
Signed-off-by: Spencer Oliver <spen@spen-soft.co.uk>

src/target/arm7tdmi.c
@@ -211,6 +211,7 @@ static int arm7tdmi_clock_data_in_endianness(struct arm_jtag *jtag_info,
 {
 	int retval = ERROR_OK;
 	struct scan_field fields[2];
+	uint8_t in_buf[4];
 
 	retval = arm_jtag_scann(jtag_info, 0x1, TAP_DRPAUSE);
 	if (retval != ERROR_OK)
@@ -223,9 +224,9 @@ static int arm7tdmi_clock_data_in_endianness(struct arm_jtag *jtag_info,
 	fields[0].out_value = NULL;
 	fields[0].in_value = NULL;
 
-	fields[1].num_bits = size * 8;
+	fields[1].num_bits = 32;
 	fields[1].out_value = NULL;
-	fields[1].in_value = in;
+	fields[1].in_value = in_buf;
 
 	jtag_add_dr_scan(jtag_info->tap, 2, fields, TAP_DRPAUSE);
 
@@ -233,7 +234,7 @@ static int arm7tdmi_clock_data_in_endianness(struct arm_jtag *jtag_info,
 		(jtag_callback_data_t)in,
 		(jtag_callback_data_t)size,
 		(jtag_callback_data_t)be,
-		(jtag_callback_data_t)in);
+		(jtag_callback_data_t)in_buf);
 
 	jtag_add_runtest(0, TAP_DRPAUSE);
 

src/target/arm9tdmi.c
@@ -263,6 +263,7 @@ int arm9tdmi_clock_data_in_endianness(struct arm_jtag *jtag_info,
 {
 	int retval = ERROR_OK;
 	struct scan_field fields[3];
+	uint8_t in_buf[4];
 
 	retval = arm_jtag_scann(jtag_info, 0x1, TAP_DRPAUSE);
 	if (retval != ERROR_OK)
@@ -272,9 +273,9 @@ int arm9tdmi_clock_data_in_endianness(struct arm_jtag *jtag_info,
 	if (retval != ERROR_OK)
 		return retval;
 
-	fields[0].num_bits = size * 8;
+	fields[0].num_bits = 32;
 	fields[0].out_value = NULL;
-	fields[0].in_value = in;
+	fields[0].in_value = in_buf;
 
 	fields[1].num_bits = 3;
 	fields[1].out_value = NULL;
@@ -290,7 +291,7 @@ int arm9tdmi_clock_data_in_endianness(struct arm_jtag *jtag_info,
 		(jtag_callback_data_t)in,
 		(jtag_callback_data_t)size,
 		(jtag_callback_data_t)be,
-		(jtag_callback_data_t)in);
+		(jtag_callback_data_t)in_buf);
 
 	jtag_add_runtest(0, TAP_DRPAUSE);
 