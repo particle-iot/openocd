0f2fd61161da4c07ab57767c9fa171739b3b7ac9
Spencer Oliver <spen@spen-soft.co.uk>
2012-10-23 16:27:27 +0100

    stlink: rename to high level interface (hla)
    This will enable us to support other high level adapters, eg, new icdi
    from TI.
    
    No configs have been changed yet, that will be in another patch.
    
    Change-Id: If29d2f8ccdd7965cb751e39c9c7e798a2ad15cd9
    Signed-off-by: Spencer Oliver <spen@spen-soft.co.uk>

--- src/jtag/hla/hla_interface.c
+++ src/jtag/hla/hla_interface.c
@@ -2,6 +2,9 @@
  *   Copyright (C) 2011 by Mathias Kuester                                 *
  *   Mathias Kuester <kesmtp@freenet.de>                                   *
  *                                                                         *
+ *   Copyright (C) 2012 by Spencer Oliver                                  *
+ *   spen@spen-soft.co.uk                                                  *
+ *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
@@ -27,36 +30,36 @@
 #include <transport/transport.h>
 #include <helper/time_support.h>
 
-#include <jtag/stlink/stlink_tcl.h>
-#include <jtag/stlink/stlink_layout.h>
-#include <jtag/stlink/stlink_transport.h>
-#include <jtag/stlink/stlink_interface.h>
+#include <jtag/hla/hla_tcl.h>
+#include <jtag/hla/hla_layout.h>
+#include <jtag/hla/hla_transport.h>
+#include <jtag/hla/hla_interface.h>
 
 #include <target/target.h>
 
-static struct stlink_interface_s stlink_if = { {0, 0, 0, 0, 0, 0}, 0, 0 };
+static struct hl_interface_s interface = { {0, 0, 0, 0, 0, 0}, 0, 0 };
 
-int stlink_interface_open(enum stlink_transports tr)
+int hl_interface_open(enum hl_transports tr)
 {
-	LOG_DEBUG("stlink_interface_open");
+	LOG_DEBUG("hl_interface_open");
 
 	/* set transport mode */
-	stlink_if.param.transport = tr;
+	interface.param.transport = tr;
 
-	return stlink_if.layout->open(&stlink_if);
+	return interface.layout->open(&interface);
 }
 
-int stlink_interface_init_target(struct target *t)
+int hl_interface_init_target(struct target *t)
 {
 	int res;
 
-	LOG_DEBUG("stlink_interface_init_target");
+	LOG_DEBUG("hl_interface_init_target");
 
 	/* this is the interface for the current target and we
 	 * can setup the private pointer in the tap structure
 	 * if the interface match the tap idcode
 	 */
-	res = stlink_if.layout->api->idcode(stlink_if.fd, &t->tap->idcode);
+	res = interface.layout->api->idcode(interface.fd, &t->tap->idcode);
 
 	if (res != ERROR_OK)
 		return res;
@@ -75,131 +78,131 @@ int stlink_interface_init_target(struct target *t)
 	}
 
 	if (found == 0) {
-		LOG_ERROR("stlink_interface_init_target: target not found: idcode: 0x%08x",
+		LOG_ERROR("hl_interface_init_target: target not found: idcode: 0x%08x",
 				t->tap->idcode);
 		return ERROR_FAIL;
 	}
 
-	t->tap->priv = &stlink_if;
+	t->tap->priv = &interface;
 	t->tap->hasidcode = 1;
 
 	return ERROR_OK;
 }
 
-static int stlink_interface_init(void)
+static int hl_interface_init(void)
 {
-	LOG_DEBUG("stlink_interface_init");
+	LOG_DEBUG("hl_interface_init");
 
 	/* here we can initialize the layout */
-	return stlink_layout_init(&stlink_if);
+	return hl_layout_init(&interface);
 }
 
-static int stlink_interface_quit(void)
+static int hl_interface_quit(void)
 {
-	LOG_DEBUG("stlink_interface_quit");
+	LOG_DEBUG("hl_interface_quit");
 
 	return ERROR_OK;
 }
 
-static int stlink_interface_speed(int speed)
+static int hl_interface_speed(int speed)
 {
-	LOG_DEBUG("stlink_interface_speed: ignore speed %d", speed);
+	LOG_DEBUG("hl_interface_speed: ignore speed %d", speed);
 
 	return ERROR_OK;
 }
 
-static int stlink_speed_div(int speed, int *khz)
+static int hl_speed_div(int speed, int *khz)
 {
 	*khz = speed;
 	return ERROR_OK;
 }
 
-static int stlink_khz(int khz, int *jtag_speed)
+static int hl_khz(int khz, int *jtag_speed)
 {
 	*jtag_speed = khz;
 	return ERROR_OK;
 }
 
-static int stlink_interface_execute_queue(void)
+static int hl_interface_execute_queue(void)
 {
-	LOG_DEBUG("stlink_interface_execute_queue: ignored");
+	LOG_DEBUG("hl_interface_execute_queue: ignored");
 
 	return ERROR_OK;
 }
 
-COMMAND_HANDLER(stlink_interface_handle_device_desc_command)
+COMMAND_HANDLER(hl_interface_handle_device_desc_command)
 {
-	LOG_DEBUG("stlink_interface_handle_device_desc_command");
+	LOG_DEBUG("hl_interface_handle_device_desc_command");
 
 	if (CMD_ARGC == 1) {
-		stlink_if.param.device_desc = strdup(CMD_ARGV[0]);
+		interface.param.device_desc = strdup(CMD_ARGV[0]);
 	} else {
 		LOG_ERROR
-		    ("expected exactly one argument to stlink_device_desc <description>");
+		    ("expected exactly one argument to hl_device_desc <description>");
 	}
 
 	return ERROR_OK;
 }
 
-COMMAND_HANDLER(stlink_interface_handle_serial_command)
+COMMAND_HANDLER(hl_interface_handle_serial_command)
 {
-	LOG_DEBUG("stlink_interface_handle_serial_command");
+	LOG_DEBUG("hl_interface_handle_serial_command");
 
 	if (CMD_ARGC == 1) {
-		stlink_if.param.serial = strdup(CMD_ARGV[0]);
+		interface.param.serial = strdup(CMD_ARGV[0]);
 	} else {
 		LOG_ERROR
-		    ("expected exactly one argument to stlink_serial <serial-number>");
+		    ("expected exactly one argument to hl_serial <serial-number>");
 	}
 
 	return ERROR_OK;
 }
 
-COMMAND_HANDLER(stlink_interface_handle_layout_command)
+COMMAND_HANDLER(hl_interface_handle_layout_command)
 {
-	LOG_DEBUG("stlink_interface_handle_layout_command");
+	LOG_DEBUG("hl_interface_handle_layout_command");
 
 	if (CMD_ARGC != 1) {
 		LOG_ERROR("Need exactly one argument to stlink_layout");
 		return ERROR_COMMAND_SYNTAX_ERROR;
 	}
 
-	if (stlink_if.layout) {
-		LOG_ERROR("already specified stlink_layout %s",
-			  stlink_if.layout->name);
-		return (strcmp(stlink_if.layout->name, CMD_ARGV[0]) != 0)
+	if (interface.layout) {
+		LOG_ERROR("already specified hl_layout %s",
+				interface.layout->name);
+		return (strcmp(interface.layout->name, CMD_ARGV[0]) != 0)
 		    ? ERROR_FAIL : ERROR_OK;
 	}
 
-	for (const struct stlink_layout *l = stlink_layout_get_list(); l->name;
+	for (const struct hl_layout *l = hl_layout_get_list(); l->name;
 	     l++) {
 		if (strcmp(l->name, CMD_ARGV[0]) == 0) {
-			stlink_if.layout = l;
+			interface.layout = l;
 			return ERROR_OK;
 		}
 	}
 
-	LOG_ERROR("No STLINK layout '%s' found", CMD_ARGV[0]);
+	LOG_ERROR("No adapter layout '%s' found", CMD_ARGV[0]);
 	return ERROR_FAIL;
 }
 
-COMMAND_HANDLER(stlink_interface_handle_vid_pid_command)
+COMMAND_HANDLER(hl_interface_handle_vid_pid_command)
 {
-	LOG_DEBUG("stlink_interface_handle_vid_pid_command");
+	LOG_DEBUG("hl_interface_handle_vid_pid_command");
 
 	if (CMD_ARGC != 2) {
 		LOG_WARNING
-		    ("ignoring extra IDs in stlink_vid_pid (maximum is 1 pair)");
+		    ("ignoring extra IDs in hl_vid_pid (maximum is 1 pair)");
 		return ERROR_COMMAND_SYNTAX_ERROR;
 	}
 
-	COMMAND_PARSE_NUMBER(u16, CMD_ARGV[0], stlink_if.param.vid);
-	COMMAND_PARSE_NUMBER(u16, CMD_ARGV[1], stlink_if.param.pid);
+	COMMAND_PARSE_NUMBER(u16, CMD_ARGV[0], interface.param.vid);
+	COMMAND_PARSE_NUMBER(u16, CMD_ARGV[1], interface.param.pid);
 
 	return ERROR_OK;
 }
 
-COMMAND_HANDLER(stlink_interface_handle_api_command)
+COMMAND_HANDLER(hl_interface_handle_api_command)
 {
 	if (CMD_ARGC != 1)
 		return ERROR_COMMAND_SYNTAX_ERROR;
@@ -209,43 +212,43 @@ COMMAND_HANDLER(stlink_interface_handle_api_command)
 	if ((new_api == 0) || (new_api > 2))
 		return ERROR_COMMAND_SYNTAX_ERROR;
 
-	stlink_if.param.api = new_api;
+	interface.param.api = new_api;
 
 	return ERROR_OK;
 }
 
-static const struct command_registration stlink_interface_command_handlers[] = {
+static const struct command_registration hl_interface_command_handlers[] = {
 	{
 	 .name = "stlink_device_desc",
-	 .handler = &stlink_interface_handle_device_desc_command,
+	 .handler = &hl_interface_handle_device_desc_command,
 	 .mode = COMMAND_CONFIG,
-	 .help = "set the stlink device description of the STLINK device",
+	 .help = "set the a device description of the adapter",
 	 .usage = "description_string",
 	 },
 	{
 	 .name = "stlink_serial",
-	 .handler = &stlink_interface_handle_serial_command,
+	 .handler = &hl_interface_handle_serial_command,
 	 .mode = COMMAND_CONFIG,
-	 .help = "set the serial number of the STLINK device",
+	 .help = "set the serial number of the adapter",
 	 .usage = "serial_string",
 	 },
 	{
 	 .name = "stlink_layout",
-	 .handler = &stlink_interface_handle_layout_command,
+	 .handler = &hl_interface_handle_layout_command,
 	 .mode = COMMAND_CONFIG,
-	 .help = "set the layout of the STLINK to usb or sg",
+	 .help = "set the layout of the adapter",
 	 .usage = "layout_name",
 	 },
 	{
 	 .name = "stlink_vid_pid",
-	 .handler = &stlink_interface_handle_vid_pid_command,
+	 .handler = &hl_interface_handle_vid_pid_command,
 	 .mode = COMMAND_CONFIG,
-	 .help = "the vendor and product ID of the STLINK device",
+	 .help = "the vendor and product ID of the adapter",
 	 .usage = "(vid pid)* ",
 	 },
 	 {
 	 .name = "stlink_api",
-	 .handler = &stlink_interface_handle_api_command,
+	 .handler = &hl_interface_handle_api_command,
 	 .mode = COMMAND_CONFIG,
 	 .help = "set the desired stlink api level",
 	 .usage = "api version 1 or 2",
@@ -253,15 +256,15 @@ static const struct command_registration stlink_interface_command_handlers[] = {
 	COMMAND_REGISTRATION_DONE
 };
 
-struct jtag_interface stlink_interface = {
+struct jtag_interface hl_interface = {
 	.name = "stlink",
 	.supported = 0,
-	.commands = stlink_interface_command_handlers,
-	.transports = stlink_transports,
-	.init = stlink_interface_init,
-	.quit = stlink_interface_quit,
-	.speed = stlink_interface_speed,
-	.speed_div = stlink_speed_div,
-	.khz = stlink_khz,
-	.execute_queue = stlink_interface_execute_queue,
+	.commands = hl_interface_command_handlers,
+	.transports = hl_transports,
+	.init = hl_interface_init,
+	.quit = hl_interface_quit,
+	.speed = hl_interface_speed,
+	.speed_div = hl_speed_div,
+	.khz = hl_khz,
+	.execute_queue = hl_interface_execute_queue,
 };
