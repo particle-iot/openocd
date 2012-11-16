211cd1043a2c090803042a058e99613a0212a810
Tomek CEDRO <cederom@tlen.pl>
2012-11-16 01:38:15 +0100

    Introducing SWD Transport files: core, libswd bridge, tcl interface. Not yet part of the build.
    Change-Id: I330193a0841fce91bbbfc972edc39a430ca0ea47
    Signed-off-by: Tomek CEDRO <cederom@tlen.pl>

---
+++ src/transport/swd.h
@@ -0,0 +1,60 @@
+/*
+ * SWD Transport Header File for OpenOCD.
+ *
+ * Copyright (C) 2011-2012 Tomasz Boleslaw CEDRO
+ * cederom@tlen.pl, http://www.tomek.cedro.info
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the Tomasz Boleslaw CEDRO nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.*
+ *
+ * Written by Tomasz Boleslaw CEDRO <cederom@tlen.pl>, 2011-2012;
+ *
+ */
+
+/** \file transport_swd.h SWD Transport Header File for OpenOCD. */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifndef OOCD_TRANSPORT_SWD_H
+#define OOCD_TRANSPORT_SWD_H
+
+int oocd_transport_swd_queue_idcode_read(struct adiv5_dap *dap, uint8_t *ack, uint32_t *data);
+int oocd_transport_swd_queue_dp_read(struct adiv5_dap *dap, unsigned reg, uint32_t *data);
+int oocd_transport_swd_queue_dp_write(struct adiv5_dap *dap, unsigned reg, uint32_t data);
+int oocd_transport_swd_queue_ap_read(struct adiv5_dap *dap, unsigned reg, uint32_t *data);
+int oocd_transport_swd_queue_ap_write(struct adiv5_dap *dap, unsigned reg, uint32_t data);
+int oocd_transport_swd_queue_ap_abort(struct adiv5_dap *dap, uint8_t *ack);
+int oocd_transport_swd_run(struct adiv5_dap *dap);
+int oocd_transport_swd_init(struct command_context *ctx);
+int oocd_transport_swd_select(struct command_context *ctx);
+int oocd_transport_swd_setup(struct command_context *ctx);
+
+extern oocd_transport_t oocd_transport_swd;
+extern const struct dap_ops *oocd_target_arm_dap_ops_swd;
+
+int oocd_transport_swd_register_commands(struct command_context *cmd_ctx);
+
+#endif
