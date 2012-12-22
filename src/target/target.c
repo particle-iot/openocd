269c1edd67e4a64f54b2cd5677b46a428dcab19f
Franck Jullien <franck.jullien@gmail.com>
2012-12-22 22:56:31 +0100

    gdb_server: Add tdesc target cmd and qXfer:features:read support v5
    This patch adds a tdesc command at target level to configure how
    the target description file must be assigned to the target.
    
    The file can be auto-generated by the target code or a path
    to an existing file can be provided.
    
    For now, target tdesc file auto-generation is not implemented.
    
    On the gdb_server, it adds support for qXfer:features:read.
    The implementation can handle large tdesc files where mutiple
    'm' answers to qXfer are needed.
    
    v4: fix coding style error
    v5: fix ignoring return value of ‘asprintf’
    
    Change-Id: Ia13414f8d35a3d5e5fe17e04f52d2e19c13d8fb7
    Signed-off-by: Franck Jullien <franck.jullien@gmail.com>

--- src/target/target.c
+++ src/target/target.c
@@ -429,6 +429,22 @@ struct target *get_current_target(struct command_context *cmd_ctx)
 	return target;
 }
 
+int target_generate_tdesc_file(struct target *target, const char *filename)
+{
+	int retval;
+
+	if (target->type->generate_tdesc_file) {
+		retval = target->type->generate_tdesc_file(target, filename);
+		if (retval != ERROR_OK)
+			return retval;
+	} else {
+		LOG_ERROR("Target has no generate_tdesc_file");
+		return ERROR_FAIL;
+	}
+
+	return ERROR_OK;
+}
+
 int target_poll(struct target *target)
 {
 	int retval;
@@ -4737,6 +4753,19 @@ static int jim_target_invoke_event(Jim_Interp *interp, int argc, Jim_Obj *const 
 	target_handle_event(target, n->value);
 	return JIM_OK;
 }
+static int jim_target_tdesc(Jim_Interp *interp, int argc, Jim_Obj * const *argv)
+{
+	Jim_GetOptInfo goi;
+	Jim_GetOpt_Setup(&goi, interp, argc - 1, argv + 1);
+	if (goi.argc != 1) {
+		const char *cmd_name = Jim_GetString(argv[0], NULL);
+		Jim_SetResultFormatted(goi.interp, "usage: %s tdesc <filename>", cmd_name);
+		return JIM_ERR;
+	}
+	struct target *target = Jim_CmdPrivData(goi.interp);
+	target->gdb_tdesc_path = strdup(Jim_GetString(argv[1], NULL));
+	return JIM_OK;
+}
 
 static const struct command_registration target_instance_command_handlers[] = {
 	{
@@ -4865,6 +4894,13 @@ static const struct command_registration target_instance_command_handlers[] = {
 		.jim_handler = jim_target_invoke_event,
 		.help = "invoke handler for specified event",
 		.usage = "event_name",
+	},
+	{
+		.name = "tdesc",
+		.mode = COMMAND_EXEC,
+		.jim_handler = jim_target_tdesc,
+		.usage = "path to tdesc file | auto",
+		.help = "Set the path to the XML target description file"
 	},
 	COMMAND_REGISTRATION_DONE
 };
@@ -5005,6 +5041,12 @@ static int target_create(Jim_GetOptInfo *goi)
 
 	cp = Jim_GetString(new_cmd, NULL);
 	target->cmd_name = strdup(cp);
+
+	/* don't use tdesc by default */
+	target->gdb_tdesc_path = NULL;
+
+	/* No pending transfert */
+	target->remaining_xfer = -1;
 
 	/* create the target specific commands */
 	if (target->type->commands) {
