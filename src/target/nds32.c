448f3653f3ed46858c41acdc39264dcdfab1ff0a
Hsiangkai Wang <hsiangkai@gmail.com>
2013-02-06 15:50:14 +0800

    nds32: support soft-reset-halt
    Support soft-reset-halt for nds32 v2
    
    Change-Id: I295310d6bbd6ecb6975b2bede5292e2fec5a9887
    Signed-off-by: Hsiangkai Wang <hsiangkai@gmail.com>

--- src/target/nds32.c
+++ src/target/nds32.c
@@ -1764,26 +1764,6 @@ int nds32_cache_sync(struct target *target, uint32_t address, uint32_t length)
 	return ERROR_OK;
 }
 
-int nds32_soft_reset_halt(struct target *target)
-{
-	struct nds32 *nds32 = target_to_nds32(target);
-	struct aice_port_s *aice = target_to_aice(target);
-
-	aice->port->api->assert_srst(AICE_SRST);
-
-	/* halt core and set pc to 0x0 */
-	int retval = target_halt(target);
-	if (retval != ERROR_OK)
-		return retval;
-
-	CHECK_RETVAL(target->type->poll(target));
-
-	/* start fetching from 0x0 */
-	nds32_set_mapped_reg(nds32, PC, 0x0);
-
-	return ERROR_OK;
-}
-
 uint32_t nds32_nextpc(struct nds32 *nds32, int current, uint32_t address)
 {
 	if (!current)
@@ -2109,8 +2089,10 @@ int nds32_assert_reset(struct target *target)
 	struct aice_port_s *aice = target_to_aice(target);
 
 	if (target->reset_halt) {
-		aice->port->api->assert_srst(AICE_RESET_HOLD);
-
+		if (nds32->soft_reset_halt)
+			target->type->soft_reset_halt(target);
+		else
+			aice->port->api->assert_srst(AICE_RESET_HOLD);
 	} else {
 		aice->port->api->assert_srst(AICE_SRST);
 		alive_sleep(nds32->boot_time);
