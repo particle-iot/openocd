ca51aaed4329111eaa4f007441a49543d7eeb505
Andrey Smirnov <andrew.smirnov@gmail.com>
2014-05-10 05:11:55 -0700

    adi_v5_cmsis_dap.c: Simplify debugging output
    Name of the function is already a part of the LOG_DEBUG macro, so
    there's no need to include it in the string itself.
    
    Change-Id: I18c3d5b746e9106d55104e490ccf5bc5e85cc380
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- src/target/adi_v5_cmsis_dap.c
+++ src/target/adi_v5_cmsis_dap.c
@@ -56,7 +56,7 @@ extern struct jtag_interface *jtag_interface;
 
 static int cmsis_dap_clear_sticky_errors(struct adiv5_dap *dap)
 {
-	LOG_DEBUG("CMSIS-ADI: %s", __func__);
+	LOG_DEBUG(" ");
 
 	const struct swd_driver *swd = jtag_interface->swd;
 	assert(swd);
@@ -67,7 +67,7 @@ static int cmsis_dap_clear_sticky_errors(struct adiv5_dap *dap)
 
 static int cmsis_dap_queue_ap_abort(struct adiv5_dap *dap, uint8_t *ack)
 {
-	LOG_DEBUG("CMSIS-ADI: %s", __func__);
+	LOG_DEBUG(" ");
 
 	const struct swd_driver *swd = jtag_interface->swd;
 	assert(swd);
@@ -78,7 +78,7 @@ static int cmsis_dap_queue_ap_abort(struct adiv5_dap *dap, uint8_t *ack)
 
 static int cmsis_dap_queue_dp_read(struct adiv5_dap *dap, unsigned reg, uint32_t *data)
 {
-	LOG_DEBUG("CMSIS-ADI: cmsis_dap_queue_dp_read %d", reg);
+	LOG_DEBUG("reg = %d", reg);
 
 	int retval = jtag_interface->swd->read_reg(
 			(CMSIS_CMD_DP | CMSIS_CMD_READ | CMSIS_CMD_A32(reg)), data);
@@ -91,7 +91,7 @@ static int cmsis_dap_queue_dp_read(struct adiv5_dap *dap, unsigned reg, uint32_t
 
 static int cmsis_dap_queue_idcode_read(struct adiv5_dap *dap, uint8_t *ack, uint32_t *data)
 {
-	LOG_DEBUG("CMSIS-ADI: cmsis_dap_queue_idcode_read");
+	LOG_DEBUG(" ");
 
 	int retval = cmsis_dap_queue_dp_read(dap, DP_IDCODE, data);
 	if (retval != ERROR_OK)
@@ -104,7 +104,7 @@ static int cmsis_dap_queue_idcode_read(struct adiv5_dap *dap, uint8_t *ack, uint
 
 static int (cmsis_dap_queue_dp_write)(struct adiv5_dap *dap, unsigned reg, uint32_t data)
 {
-	LOG_DEBUG("CMSIS-ADI: cmsis_dap_queue_dp_write %d 0x%08" PRIx32, reg, data);
+	LOG_DEBUG("reg = %d, data = 0x%08" PRIx32, reg, data);
 
 	/* setting the ORUNDETECT bit causes issues for some targets,
 	 * disable until we find out why */
@@ -142,7 +142,7 @@ static int (cmsis_dap_queue_ap_read)(struct adiv5_dap *dap, unsigned reg, uint32
 	if (retval != ERROR_OK)
 		return retval;
 
-	LOG_DEBUG("CMSIS-ADI: cmsis_dap_queue_ap_read %d", reg);
+	LOG_DEBUG("reg = %d", reg);
 
 	retval = jtag_interface->swd->read_reg(
 			(CMSIS_CMD_AP | CMSIS_CMD_READ | CMSIS_CMD_A32(reg)), data);
@@ -166,7 +166,7 @@ static int (cmsis_dap_queue_ap_write)(struct adiv5_dap *dap, unsigned reg, uint3
 	if (retval != ERROR_OK)
 		return retval;
 
-	LOG_DEBUG("CMSIS-ADI: cmsis_dap_queue_ap_write %d 0x%08" PRIx32, reg, data);
+	LOG_DEBUG("reg = %d, data = 0x%08" PRIx32, reg, data);
 
 	retval = jtag_interface->swd->write_reg(
 			(CMSIS_CMD_AP | CMSIS_CMD_WRITE | CMSIS_CMD_A32(reg)), data);
@@ -180,7 +180,7 @@ static int (cmsis_dap_queue_ap_write)(struct adiv5_dap *dap, unsigned reg, uint3
 static int (cmsis_dap_queue_ap_read_block)(struct adiv5_dap *dap, unsigned reg,
 		uint32_t blocksize, uint8_t *buffer)
 {
-	LOG_DEBUG("CMSIS-ADI: cmsis_dap_queue_ap_read_block 0x%08" PRIx32, blocksize);
+	LOG_DEBUG("blocksize = 0x%08" PRIx32, blocksize);
 
 	int retval = jtag_interface->swd->read_block(
 			(CMSIS_CMD_AP | CMSIS_CMD_READ | CMSIS_CMD_A32(AP_REG_DRW)),
@@ -270,7 +270,7 @@ static const struct command_registration cmsis_dap_handlers[] = {
 
 static int cmsis_dap_select(struct command_context *ctx)
 {
-	LOG_DEBUG("CMSIS-ADI: cmsis_dap_select");
+	LOG_DEBUG(" ");
 
 	int retval = register_commands(ctx, NULL, cmsis_dap_handlers);
 
