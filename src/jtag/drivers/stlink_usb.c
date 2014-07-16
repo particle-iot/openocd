9e19e055dcb5a46442fda2387b9ec3b1d0990b60
Andrey Skvortsov <andrej.skvortzov@gmail.com>
2014-07-16 15:58:03 +0400

    jtag: drivers: stlink: fix problem with breaking SWO trace functionality
    1) If the SWO trace was enabled, then the reset run/halt commands broke
    the SWO trace functionality. ST-Link received wrong data afterwards
    until OpenOCD is restarted.
    Disabling and reenabling tracing after any reset fixes this problem.
    
    2) The second problem was that the trace functionality was broken after the
    first execution of openocd regardless of usage of tracing.
    The problem is that trace USB endpoint (83) is broken (-EPIPE) after
    setting up second time USB configuration or USB reset command.
    To fix this user should replug st-link.
    
    Tested on STLINK v2 JTAG v21 with LDM-K1986BE92QI development board.
    
    Change-Id: I11bf4eaec00169cdc1b7f79916218e576eccf68a
    Signed-off-by: Andrey Skvortsov <andrej.skvortzov@gmail.com>

--- src/jtag/drivers/stlink_usb.c
+++ src/jtag/drivers/stlink_usb.c
@@ -222,6 +222,8 @@ enum stlink_mode {
 #define REQUEST_SENSE_LENGTH 18
 
 static void stlink_usb_init_buffer(void *handle, uint8_t direction, uint32_t size);
+static int stlink_usb_trace_enable(void *handle);
+static void stlink_usb_trace_disable(void *handle);
 
 /** */
 static int stlink_usb_xfer_v1_get_status(void *handle)
@@ -942,11 +944,22 @@ static int stlink_usb_reset(void *handle)
 
 	LOG_DEBUG("RESET: 0x%08X", h->databuf[0]);
 
-	/* the following is not a error under swd (using hardware srst), so return success */
-	if (h->databuf[0] == STLINK_SWD_AP_WAIT || h->databuf[0] == STLINK_SWD_DP_WAIT)
-		return ERROR_OK;
+	/* the following is not a error under swd (using hardware srst) */
+	if (h->databuf[0] == STLINK_SWD_AP_WAIT
+		|| h->databuf[0] == STLINK_SWD_DP_WAIT
+		|| h->databuf[0] == STLINK_DEBUG_ERR_OK) {
 
-	return h->databuf[0] == STLINK_DEBUG_ERR_OK ? ERROR_OK : ERROR_FAIL;
+		/* Try to restart tracing, if it's used */
+		if (h->jtag_api == STLINK_JTAG_API_V2 && h->trace.source_hz) {
+			stlink_usb_trace_disable(h);
+			if (stlink_usb_trace_enable(h) == ERROR_OK)
+				LOG_DEBUG("Tracing: restarted");
+			else
+				LOG_ERROR("Tracing: restart failed");
+		}
+		return ERROR_OK;
+	} else
+		return ERROR_FAIL;
 }
 
 static int stlink_usb_assert_srst(void *handle, int srst)
@@ -1621,8 +1634,6 @@ static int stlink_usb_open(struct hl_interface_param_s *param, void **fd)
 			LOG_ERROR("open failed");
 			goto error_open;
 		}
-
-		jtag_libusb_set_configuration(h->fd, 0);
 
 		if (jtag_libusb_claim_interface(h->fd, 0) != ERROR_OK) {
 			LOG_DEBUG("claim interface failed");
