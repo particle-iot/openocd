057cb914dda8e43707cd5f7ff7ad5df80b67bd9b
Peter Mamonov <pmamonov@gmail.com>
2014-09-23 12:51:05 +0400

    mips64: Add generic mips64 target support
    Change-Id: I2bdd2573f23e65652686d18031698f423eec77c0
    Signed-off-by: Konstantin Kostyukhin <kost@niisi.msk.ru>
    Signed-off-by: Aleksey Kuleshov <rndfax@yandex.ru>
    Signed-off-by: Peter Mamonov <pmamonov@gmail.com>
    CC: Dongxue Zhang <elta.era@gmail.com>
    CC: Oleksij Rempel <linux@rempel-privat.de>
    CC: Paul Fertser <fercerpav@gmail.com>
    CC: Salvador Arroyo <sarroyofdez@yahoo.es>
    CC: Spencer Oliver <spen@spen-soft.co.uk>

---
+++ src/target/mips_mips64.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *   MIPS64 generic target support                                         *
+ *                                                                         *
+ *   Copyright (C) 2014 by Andrey Sidorov <anysidorov@gmail.com>           *
+ *   Copyright (C) 2014 by Aleksey Kuleshov <rndfax@yandex.ru>             *
+ *   Copyright (C) 2014 by Peter Mamonov <pmamonov@gmail.com>              *
+ *                                                                         *
+ *   Based on the work of:                                                 *
+ *       Copyright (C) 2008 by Spencer Oliver                              *
+ *       Copyright (C) 2008 by David T.L. Wong                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#ifndef MIPS_MIPS64_H
+#define MIPS_MIPS64_H
+
+#include "helper/types.h"
+
+typedef struct mips_mips64_common_s {
+	int common_magic;
+	mips64_common_t mips64_common;
+} mips_mips64_common_t;
+
+#endif /* MIPS_MIPS64_H */
