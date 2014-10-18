0cbb0e96cfb098df5c338d6761f1f15c4df3c359
Paul Fertser <fercerpav@gmail.com>
2014-10-18 15:34:12 +0400

    rtos: add instructions and helper code to make FreeRTOS work again
    Run-time tested with FreeRTOS V8.1.2 (current version).
    
    For the time being I propose this way of dealing with RTOSes that do
    not export necessary information on their own.
    
    I also suggest implementing a similar scheme for ChibiOS, exporting
    the necessary struct fields' offsets via an OpenOCD-specific helper.
    
    Change-Id: Iacf8b88004d62206215fe80011fd7592438446a3
    Signed-off-by: Paul Fertser <fercerpav@gmail.com>

---
+++ contrib/rtos-helpers/FreeRTOS-openocd.c
@@ -0,0 +1,14 @@
+/*
+ * Since at least FreeRTOS V7.5.3 uxTopUsedPriority is no longer
+ * present in the kernel, so it has to be supplied by other means for
+ * OpenOCD's threads awareness.
+ *
+ * Add this file to your project, and, if you're using --gc-sections,
+ * ``--undefined=uxTopUsedPriority'' (or
+ * ``-Wl,--undefined=uxTopUsedPriority'' when using gcc for final
+ * linking) to your LDFLAGS.
+ */
+
+#include "FreeRTOSConfig.h"
+
+const int __attribute__((used)) uxTopUsedPriority = configMAX_PRIORITIES;
