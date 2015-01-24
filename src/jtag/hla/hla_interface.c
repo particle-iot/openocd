particle-iot/openocd

commit ab0432176c05d2c6109619bdb1d9944ee858334b

hla: add ability to change adapter speed (if supported)
As a note we need to cache the requested speed setting, as the
hla interface may not be ready when the first adapter_khz is called.

Change-Id: I2fa6807d5f0bd3f0365cf178bd10a230c39415a7
Signed-off-by: Spencer Oliver <spen@spen-soft.co.uk>
Reviewed-on: http://openocd.zylin.com/2334
Tested-by: jenkins

src/jtag/hla/hla_interface.c
@@ -37,7 +37,7 @@
 
 #include <target/target.h>
 
-static struct hl_interface_s hl_if = { {0, 0, 0, 0, 0, HL_TRANSPORT_UNKNOWN, false, NULL, 0}, 0, 0 };
+static struct hl_interface_s hl_if = { {0, 0, 0, 0, 0, HL_TRANSPORT_UNKNOWN, false, NULL, 0, -1}, 0, 0 };
 
 int hl_interface_open(enum hl_transports tr)
 {
@@ -148,20 +148,31 @@ int hl_interface_init_reset(void)
 	return ERROR_OK;
 }
 
-static int dummy_khz(int khz, int *jtag_speed)
+static int hl_interface_khz(int khz, int *jtag_speed)
 {
-	*jtag_speed = khz;
+	*jtag_speed = hl_if.layout->api->speed(hl_if.handle, khz, true);
 	return ERROR_OK;
 }
 
-static int dummy_speed_div(int speed, int *khz)
+static int hl_interface_speed_div(int speed, int *khz)
 {
 	*khz = speed;
 	return ERROR_OK;
 }
 
-static int dummy_speed(int speed)
+static int hl_interface_speed(int speed)
 {
+	if (hl_if.layout->api->speed == NULL)
+		return ERROR_OK;
+
+	if (hl_if.handle == NULL) {
+		/* pass speed as initial param as interface not open yet */
+		hl_if.param.initial_interface_speed = speed;
+		return ERROR_OK;
+	}
+
+	hl_if.layout->api->speed(hl_if.handle, speed, false);
+
 	return ERROR_OK;
 }
 
@@ -340,7 +351,7 @@ struct jtag_interface hl_interface = {
 	.init = hl_interface_init,
 	.quit = hl_interface_quit,
 	.execute_queue = hl_interface_execute_queue,
-	.speed = &dummy_speed,
-	.khz = &dummy_khz,
-	.speed_div = &dummy_speed_div,
+	.speed = &hl_interface_speed,
+	.khz = &hl_interface_khz,
+	.speed_div = &hl_interface_speed_div,
 };

src/jtag/hla/hla_interface.h
@@ -50,6 +50,8 @@ struct hl_interface_param_s {
 	FILE *trace_f;
 	/** Trace module source clock rate */
 	uint32_t trace_source_hz;
+	/** Initial interface clock clock speed */
+	int initial_interface_speed;
 };
 
 struct hl_interface_s {

src/jtag/hla/hla_layout.h
@@ -78,6 +78,8 @@ struct hl_layout_api_s {
 	/** */
 	int (*custom_command) (void *handle, const char *command);
 	/** */
+	int (*speed)(void *handle, int khz, bool query);
+	/** */
 	enum target_state (*state) (void *fd);
 };
 

commit 74d087d11d7cf31d2cdc4bc30c500da17eff9bc5

hla: check if the layout API doesn't implement speed() in hl_interface_khz()
Fix a bug introduced in commit ab0432176c05d2c6109619bdb1d9944ee858334b
("hla: add ability to change adapter speed (if supported)") where openocd
would segfault if we try to get the adapter speed when the layout API
speed() method is not set.

Change-Id: I5d8ba57ea9507634801af0fd6cfe7f557fcdfaf0
Signed-off-by: Artemiy Volkov <artemiyv@acm.org>

src/jtag/hla/hla_interface.c
@@ -150,6 +150,9 @@ int hl_interface_init_reset(void)
 
 static int hl_interface_khz(int khz, int *jtag_speed)
 {
+	if (hl_if.layout->api->speed == NULL)
+		return ERROR_OK;
+
 	*jtag_speed = hl_if.layout->api->speed(hl_if.handle, khz, true);
 	return ERROR_OK;
 }