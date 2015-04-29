272f38472ea662659cdd120add10127d8ba77700
Chris Pavlina <Pavlina.Chris@gmail.com>
2015-04-29 14:24:06 -0400

    flash/nor/at91sam4.c: Add support for Atmel SAM4S4C
    Change-Id: I72bda1e23fc62c5be2d27b35203cd77ec402cec2
    Signed-off-by: Chris Pavlina <Pavlina.Chris@gmail.com>

--- src/flash/nor/at91sam4.c
+++ src/flash/nor/at91sam4.c
@@ -396,6 +396,39 @@ static const struct sam4_chip_details all_sam4_details[] = {
 		  },
 		},
 	},
+	/*atsam4s4c - LQFP100/BGA100*/
+	{
+		.chipid_cidr    = 0x28AB09E0,
+		.name           = "at91sam4s4c",
+		.total_flash_size     = 256 * 1024,
+		.total_sram_size      =  64 * 1024,
+		.n_gpnvms       = 2,
+		.n_banks        = 1,
+		{
+/*		.bank[0] = {*/
+		  {
+			.probed = 0,
+			.pChip  = NULL,
+			.pBank  = NULL,
+			.bank_number = 0,
+			.base_address = FLASH_BANK_BASE_S,
+			.controller_address = 0x400e0a00,
+			.flash_wait_states = 6,	/* workaround silicon bug */
+			.present = 1,
+			.size_bytes =  512 * 1024,
+			.nsectors   =  32,
+			.sector_size = 8192,
+			.page_size   = 512,
+		  },
+/*		.bank[1] = {*/
+		  {
+			.present = 0,
+			.probed = 0,
+			.bank_number = 1,
+
+		  },
+		},
+	},
 	/*atsam4s8c - LQFP100/BGA100*/
 	{
 		.chipid_cidr    = 0x28AC0AE0,
