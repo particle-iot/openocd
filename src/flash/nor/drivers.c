483fe436fb2a66fcc212df1ea4d130045d1c1cd1
Rémi PRUD’HOMME <prudhomme.remi@gmail.com>
2015-04-30 16:44:25 +0200

    [New] flash/nor/stm32f7x.c
    little typo fix.
    Remove the x attribute.
    
    Change-Id: Ia99504b600bdeabc6f9a1cac9c0c2d7aa2494878
    Signed-off-by: Rémi PRUD'HOMME <prudhomme.remi@gmail.com>

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -40,6 +40,7 @@ extern struct flash_driver str9xpec_flash;
 extern struct flash_driver stm32f1x_flash;
 extern struct flash_driver stm32f2x_flash;
 extern struct flash_driver stm32lx_flash;
+extern struct flash_driver stm32f7x_flash;
 extern struct flash_driver tms470_flash;
 extern struct flash_driver ocl_flash;
 extern struct flash_driver pic32mx_flash;
@@ -83,6 +84,7 @@ static struct flash_driver *flash_drivers[] = {
 	&stm32f1x_flash,
 	&stm32f2x_flash,
 	&stm32lx_flash,
+	&stm32f7x_flash,
 	&tms470_flash,
 	&ocl_flash,
 	&pic32mx_flash,
