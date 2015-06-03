5d0689008f19a843aaa1407e7e12979466333f22
Vianney le Clément de Saint-Marcq <vianney.leclement@essensium.com>
2015-06-03 09:21:46 +0200

    rtos/ThreadX: Add support for ARM926EJ-S
    Add ThreadX support for arm926ejs target.  This patch was orginally
    written and submitted by Alexander Drozdov on the openocd-devel mailing
    list on 2015-01-29 [1].
    
    There are two types of stacking, depending on the execution context:
    * Normal, saving only R4-R11, R14, and CPSR registers, and
    * Interrupt, saving all registers.
    
    The context is indicated by the first 32-bit word on the stack: 0x0 for
    a normal context, 0x1 for an interrupt context.
    
    The patch has been tested on a Cypress FX3 chip.
    
    [1] http://sourceforge.net/p/openocd/mailman/message/33287429/
    
    Change-Id: Icb2ed10df32e20e618fdafc7f91cb2abfb4d910f
    Signed-off-by: Vianney le Clément de Saint-Marcq <vianney.leclement@essensium.com>
    Cc: Alexander Drozdov <adrozdoff@gmail.com>

--- src/rtos/rtos_standard_stackings.h
+++ src/rtos/rtos_standard_stackings.h
@@ -30,5 +30,6 @@
 extern const struct rtos_register_stacking rtos_standard_Cortex_M3_stacking;
 extern const struct rtos_register_stacking rtos_standard_Cortex_R4_stacking;
 extern const struct rtos_register_stacking rtos_standard_NDS32_N1068_stacking;
+extern const struct rtos_register_stacking rtos_standard_arm926ejs_stackings[2];
 
 #endif	/* ifndef INCLUDED_RTOS_STANDARD_STACKINGS_H_ */
