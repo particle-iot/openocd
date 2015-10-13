099472f197ce922a81db878053f481185bdb66a9
Bogdan Kolbov <kolbov@niiet.ru>
2015-10-13 09:19:25 +0300

    niietcm4: support for NIIET's Cortex-M4 microcontroller internal flash memory
    This adds docs, example config, flash driver for russian Cortex-M4 microcontroller.
    Driver is only supports K1921VK01T model for now.
    
    Change-Id: I135259bb055dd2df1a17de99f066e2b24eae1b0f
    Signed-off-by: Bogdan Kolbov <kolbov@niiet.ru>

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -60,6 +60,8 @@ extern struct flash_driver mrvlqspi_flash;
 extern struct flash_driver psoc4_flash;
 extern struct flash_driver sim3x_flash;
 extern struct flash_driver jtagspi_flash;
+extern struct flash_driver niietcm4_flash;
+
 
 /**
  * The list of built-in flash drivers.
@@ -104,6 +106,7 @@ static struct flash_driver *flash_drivers[] = {
 	&psoc4_flash,
 	&sim3x_flash,
 	&jtagspi_flash,
+	&niietcm4_flash,
 	NULL,
 };
 
