709f7dd8333d5bc6320e406e352cca2f816b58a6
Matthias Welwarsky <matthias@welwarsky.de>
2015-10-18 13:50:58 +0200

    cortex_a: Update instruction cache after setting a soft breakpoint
    Call armv7a_di_cache_clean_inval_virt() after writing the breakpoint
    to memory to make the change visible to the CPU.
    
    Change-Id: I24fc27058d99cb00d7f6002ccb623cab66b0d234
    Signed-off-by: Matthias Welwarsky <matthias@welwarsky.de>

--- src/target/cortex_a.c
+++ src/target/cortex_a.c
@@ -1475,6 +1475,10 @@ static int cortex_a_set_breakpoint(struct target *target,
 				breakpoint->length, 1, code);
 		if (retval != ERROR_OK)
 			return retval;
+
+		/* update i-cache at breakpoint location */
+		armv7a_di_cache_clean_inval_virt(target, breakpoint->address, breakpoint->length);
+
 		breakpoint->set = 0x11;	/* Any nice value but 0 */
 	}
 
