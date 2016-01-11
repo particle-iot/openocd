97d31a31f5830a93dbdb76a30c4b2ca242ecd9a6
Jiri Kastner <cz172638@gmail.com>
2016-01-11 23:21:34 +0100

    dap_dp_init: fixed CSYSPWRUPACK poll for some more complicated soc designs
    in some complex designs, like APQ8064, we will never get CSYSPWRUPACK, for following reasons:
     - some parts of soc are 'hidden' even from debug port
     - not all cores are up, when bootloader is waiting for boot image
    
    tested on
     - APQ8064
     - Nvidia Tegra K1 (Cortex-A15)
     - TMS57004 (Cortex-R4)
     - TMS570LC43x (Cortex-R5)
     - Olimex STM32-H405 (Cortex-M4)
    
    Change-Id: I626ad43d4e126dd42a016fe2add19a3ff7acb0ba
    Signed-off-by: Jiri Kastner <cz172638@gmail.com>
    Tested-by: Jiri Kastner <cz172638@gmail.com>

--- src/target/arm_adi_v5.c
+++ src/target/arm_adi_v5.c
@@ -614,20 +614,16 @@ int dap_dp_init(struct adiv5_dap *dap)
 	for (size_t i = 0; i < 10; i++) {
 		/* DP initialization */
 
-		retval = dap_queue_dp_read(dap, DP_CTRL_STAT, NULL);
+		retval = dap_dp_read_atomic(dap, DP_CTRL_STAT, NULL);
 		if (retval != ERROR_OK)
 			continue;
 
-		retval = dap_queue_dp_write(dap, DP_CTRL_STAT, SSTICKYERR);
-		if (retval != ERROR_OK)
-			continue;
-
-		retval = dap_queue_dp_read(dap, DP_CTRL_STAT, NULL);
+		retval = dap_dp_write_atomic(dap, DP_CTRL_STAT, SSTICKYERR);
 		if (retval != ERROR_OK)
 			continue;
 
 		dap->dp_ctrl_stat = CDBGPWRUPREQ | CSYSPWRUPREQ;
-		retval = dap_queue_dp_write(dap, DP_CTRL_STAT, dap->dp_ctrl_stat);
+		retval = dap_dp_write_atomic(dap, DP_CTRL_STAT, dap->dp_ctrl_stat);
 		if (retval != ERROR_OK)
 			continue;
 
@@ -644,22 +640,18 @@ int dap_dp_init(struct adiv5_dap *dap)
 					      CSYSPWRUPACK, CSYSPWRUPACK,
 					      DAP_POWER_DOMAIN_TIMEOUT);
 		if (retval != ERROR_OK)
-			continue;
+			LOG_DEBUG("DAP: let's continue without CSYSPWRUPACK");
 
-		retval = dap_queue_dp_read(dap, DP_CTRL_STAT, NULL);
+		retval = dap_dp_read_atomic(dap, DP_CTRL_STAT, &dap->dp_ctrl_stat);
 		if (retval != ERROR_OK)
 			continue;
 
 		/* With debug power on we can activate OVERRUN checking */
-		dap->dp_ctrl_stat = CDBGPWRUPREQ | CSYSPWRUPREQ | CORUNDETECT;
-		retval = dap_queue_dp_write(dap, DP_CTRL_STAT, dap->dp_ctrl_stat);
+		dap->dp_ctrl_stat |= CORUNDETECT;
+		retval = dap_dp_write_atomic(dap, DP_CTRL_STAT, dap->dp_ctrl_stat);
 		if (retval != ERROR_OK)
 			continue;
-		retval = dap_queue_dp_read(dap, DP_CTRL_STAT, NULL);
-		if (retval != ERROR_OK)
-			continue;
-
-		retval = dap_run(dap);
+		retval = dap_dp_read_atomic(dap, DP_CTRL_STAT, &dap->dp_ctrl_stat);
 		if (retval != ERROR_OK)
 			continue;
 
