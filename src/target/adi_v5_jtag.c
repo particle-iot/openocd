a2eae57031fd3af532f78672310d38ba22fc95ac
Jiri Kastner <cz172638@gmail.com>
2016-01-20 12:24:27 +0100

    dap_dp_init: fixed CSYSPWRUPACK poll for some more complicated soc designs
    in some complex designs, like APQ8064, we will never get CSYSPWRUPACK, for following reasons:
     - some parts of soc are 'hidden' even from debug port
     - not all cores are up, when bootloader is waiting for boot image
    
    adiv5_dap: introduce csysprwupack_ignore
    
    adi_v5_jtag: fix check for CSYSPWRUPACK in jtagdp_transaction_endcheck()
     - use csyspwrupack_ignore to check power domains.
    
    tested on
     - APQ8064
     - Nvidia Tegra K1 (Cortex-A15)
     - TMS57004 (Cortex-R4)
     - TMS570LC43x (Cortex-R5)
     - Olimex STM32-H405 (Cortex-M4)
    
    Change-Id: I626ad43d4e126dd42a016fe2add19a3ff7acb0ba
    Signed-off-by: Jiri Kastner <cz172638@gmail.com>
    Tested-by: Jiri Kastner <cz172638@gmail.com>

--- src/target/adi_v5_jtag.c
+++ src/target/adi_v5_jtag.c
@@ -270,9 +270,13 @@ static int jtagdp_transaction_endcheck(struct adiv5_dap *dap)
 	/* Check for STICKYERR and STICKYORUN */
 	if (ctrlstat & (SSTICKYORUN | SSTICKYERR)) {
 		LOG_DEBUG("jtag-dp: CTRL/STAT error, 0x%" PRIx32, ctrlstat);
-		/* Check power to debug regions */
+                /* Check power to debug regions */
+		bool some_pwr_missing, with_ignored_csysack_pwr_missing = false;
 		if ((ctrlstat & (CDBGPWRUPREQ | CDBGPWRUPACK | CSYSPWRUPREQ | CSYSPWRUPACK)) !=
-						(CDBGPWRUPREQ | CDBGPWRUPACK | CSYSPWRUPREQ | CSYSPWRUPACK)) {
+						(CDBGPWRUPREQ | CDBGPWRUPACK | CSYSPWRUPREQ | CSYSPWRUPACK)) some_pwr_missing = true;
+		if (((ctrlstat & (CDBGPWRUPREQ | CDBGPWRUPACK | CSYSPWRUPREQ | CSYSPWRUPACK)) ==
+						(CDBGPWRUPREQ | CDBGPWRUPACK | CSYSPWRUPREQ)) && (!dap->csysprwupack_ignore)) with_ignored_csysack_pwr_missing = true;
+		if (some_pwr_missing || with_ignored_csysack_pwr_missing) {
 			LOG_ERROR("Debug regions are unpowered, an unexpected reset might have happened");
 			return ERROR_JTAG_DEVICE_ERROR;
 		} else {
