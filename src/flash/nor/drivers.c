f31d980ba3322509d89a69a0e37c9fb053e60669
Andreas Färber <afaerber@suse.de>
2016-05-03 23:47:54 +0200

    psoc5lp: Add NV Latch flash driver [WIP]
    Tested on CY8CKIT-059, except modifying-write.
    
    Change-Id: I6e920ed930dcd5c7f0b10c5b1b4791a828d9080a
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -55,6 +55,7 @@ extern struct flash_driver pic32mx_flash;
 extern struct flash_driver psoc4_flash;
 extern struct flash_driver psoc5lp_flash;
 extern struct flash_driver psoc5lp_eeprom_flash;
+extern struct flash_driver psoc5lp_nvl_flash;
 extern struct flash_driver sim3x_flash;
 extern struct flash_driver stellaris_flash;
 extern struct flash_driver stm32f1x_flash;
@@ -107,6 +108,7 @@ static struct flash_driver *flash_drivers[] = {
 	&psoc4_flash,
 	&psoc5lp_flash,
 	&psoc5lp_eeprom_flash,
+	&psoc5lp_nvl_flash,
 	&sim3x_flash,
 	&stellaris_flash,
 	&stm32f1x_flash,
