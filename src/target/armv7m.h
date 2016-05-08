b8789bc688af5adb836c19913e5be67c92c4491f
Andreas Färber <afaerber@suse.de>
2016-05-08 23:49:07 +0200

    armv7m: Merge 0xff and 0x00 based blank checks
    Rename armv7m_blank_check_memory() to armv7m_ff_blank_check_memory().
    Re-introduce a new parametrized armv7m_blank_check_memory() and derived
    armv7m_0_blank_check_memory() to replace xmc4xxx_blank_check_memory().
    
    Change-Id: Ifc34712714488a1d377fd4424c5e8a148616598f
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- src/target/armv7m.h
+++ src/target/armv7m.h
@@ -224,7 +224,9 @@ int armv7m_restore_context(struct target *target);
 
 int armv7m_checksum_memory(struct target *target,
 		uint32_t address, uint32_t count, uint32_t *checksum);
-int armv7m_blank_check_memory(struct target *target,
+int armv7m_ff_blank_check_memory(struct target *target,
+		uint32_t address, uint32_t count, uint32_t *blank);
+int armv7m_0_blank_check_memory(struct target *target,
 		uint32_t address, uint32_t count, uint32_t *blank);
 
 int armv7m_maybe_skip_bkpt_inst(struct target *target, bool *inst_found);
