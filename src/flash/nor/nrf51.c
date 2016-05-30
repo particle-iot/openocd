12e1bcf46caad126fe1cb1a4e13c59c4afb721c8
Michael Dietz <mjdietzx@gmail.com>
2016-05-30 01:54:47 -0700

    nRF51: Fix bug in Code memory size.
    The code memory size was a bug and seemed to be working by
    accident since there happened to be 256 pages in the device that was
    tested on which corresponded to 256kb.
    
    Change-Id: I4e98f7498a36c53fc51783eddfdaba704d30e3ca
    Signed-off-by: Michael Dietz <mjdietzx@gmail.com>

--- src/flash/nor/nrf51.c
+++ src/flash/nor/nrf51.c
@@ -623,6 +623,24 @@ static int nrf51_probe(struct flash_bank *bank)
 		return res;
 	}
 
+	res = target_read_u32(chip->target, NRF51_FICR_CODEPAGESIZE,
+			&chip->code_page_size);
+	if (res != ERROR_OK) {
+		LOG_ERROR("Couldn't read code page size");
+		return res;
+	}
+
+	res = target_read_u32(chip->target, NRF51_FICR_CODESIZE,
+			&chip->code_memory_size);
+	if (res != ERROR_OK) {
+		LOG_ERROR("Couldn't read code memory size");
+		return res;
+	}
+
+	/* The value stored in NRF51_FICR_CODESIZE is actually the number of bytes in one page of FLASH,
+	   so to get the total size of Code FLASH we need to multiply that by the number of pages in Code FLASH. */
+	chip->code_memory_size = (chip->code_memory_size * chip->code_page_size) / 1024;
+
 	hwid &= 0xFFFF;	/* HWID is stored in the lower two
 			 * bytes of the CONFIGID register */
 
@@ -643,25 +661,6 @@ static int nrf51_probe(struct flash_bank *bank)
 
 
 	if (bank->base == NRF51_FLASH_BASE) {
-		res = target_read_u32(chip->target, NRF51_FICR_CODEPAGESIZE,
-				      &chip->code_page_size);
-		if (res != ERROR_OK) {
-			LOG_ERROR("Couldn't read code page size");
-			return res;
-		}
-
-		res = target_read_u32(chip->target, NRF51_FICR_CODESIZE,
-				      &chip->code_memory_size);
-		if (res != ERROR_OK) {
-			LOG_ERROR("Couldn't read code memory size");
-			return res;
-		}
-
-		if (spec && chip->code_memory_size != spec->flash_size_kb) {
-			LOG_ERROR("Chip's reported Flash capacity does not match expected one");
-			return ERROR_FAIL;
-		}
-
 		bank->size = chip->code_memory_size * 1024;
 		bank->num_sectors = bank->size / chip->code_page_size;
 		bank->sectors = calloc(bank->num_sectors,
@@ -1272,7 +1271,7 @@ static int nrf51_info(struct flash_bank *bank, char *buf, int buf_size)
 		 "reset value for XTALFREQ: %"PRIx32"\n"
 		 "firmware id: 0x%04"PRIx32,
 		 ficr[0].value,
-		 ficr[1].value,
+		 (ficr[1].value * ficr[0].value) / 1024,
 		 (ficr[2].value == 0xFFFFFFFF) ? 0 : ficr[2].value / 1024,
 		 ((ficr[3].value & 0xFF) == 0x00) ? "present" : "not present",
 		 ficr[4].value,
