particle-iot/openocd

commit cd440bd32a120a9b4c2d703d3d16dd52f16edab2

add armv7a_cache handlers
This patch introduces, new command set and handlers for l1 and l2x caches.

Patch set 10 folded the following changes into this one:

Ib1a2a1fc1b929dc49532ac13a78e8eb796ab4415
If8d87a03281d0f4ad402909998e7834eb4837e79
I0749f129fa74e04f4e9c20d143a744f09ef750d8

Change-Id: I849f4d1f20610087885eeddefa81d976f77cf199
Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>
Signed-off-by: Matthias Welwarsky <matthias@welwarsky.de>
Reviewed-on: http://openocd.zylin.com/2800
Tested-by: jenkins
Reviewed-by: Paul Fertser <fercerpav@gmail.com>

src/target/armv7a_cache_l2x.c
@@ -1,0 +1,375 @@
+/***************************************************************************
+ *   Copyright (C) 2015 by Oleksij Rempel                                  *
+ *   linux@rempel-privat.de                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "jtag/interface.h"
+#include "arm.h"
+#include "armv7a.h"
+#include "armv7a_cache.h"
+#include <helper/time_support.h>
+#include "target.h"
+#include "target_type.h"
+
+static int arm7a_l2x_sanity_check(struct target *target)
+{
+	struct armv7a_common *armv7a = target_to_armv7a(target);
+	struct armv7a_l2x_cache *l2x_cache = (struct armv7a_l2x_cache *)
+		(armv7a->armv7a_mmu.armv7a_cache.l2_cache);
+
+	if (target->state != TARGET_HALTED) {
+		LOG_ERROR("%s: target not halted", __func__);
+		return ERROR_TARGET_NOT_HALTED;
+	}
+
+	if (!l2x_cache || !l2x_cache->base) {
+		LOG_DEBUG("l2x is not configured!");
+		return ERROR_FAIL;
+	}
+
+	return ERROR_OK;
+}
+/*
+ * clean and invalidate complete l2x cache
+ */
+static int arm7a_l2x_flush_all_data(struct target *target)
+{
+	struct armv7a_common *armv7a = target_to_armv7a(target);
+	struct armv7a_l2x_cache *l2x_cache = (struct armv7a_l2x_cache *)
+		(armv7a->armv7a_mmu.armv7a_cache.l2_cache);
+	uint32_t l2_way_val;
+	int retval;
+
+	retval = arm7a_l2x_sanity_check(target);
+	if (retval)
+		return retval;
+
+	l2_way_val = (1 << l2x_cache->way) - 1;
+
+	return target_write_phys_memory(target,
+			l2x_cache->base + L2X0_CLEAN_INV_WAY,
+			4, 1, (uint8_t *)&l2_way_val);
+}
+
+int armv7a_l2x_cache_flush_virt(struct target *target, uint32_t virt,
+					uint32_t size)
+{
+	struct armv7a_common *armv7a = target_to_armv7a(target);
+	struct armv7a_l2x_cache *l2x_cache = (struct armv7a_l2x_cache *)
+		(armv7a->armv7a_mmu.armv7a_cache.l2_cache);
+	/* FIXME: different controllers have different linelen? */
+	uint32_t i, linelen = 32;
+	int retval;
+
+	retval = arm7a_l2x_sanity_check(target);
+	if (retval)
+		return retval;
+
+	for (i = 0; i < size; i += linelen) {
+		uint32_t pa, offs = virt + i;
+
+		/* FIXME: use less verbose virt2phys? */
+		retval = target->type->virt2phys(target, offs, &pa);
+		if (retval != ERROR_OK)
+			goto done;
+
+		retval = target_write_phys_memory(target,
+				l2x_cache->base + L2X0_CLEAN_INV_LINE_PA,
+				4, 1, (uint8_t *)&pa);
+		if (retval != ERROR_OK)
+			goto done;
+	}
+	return retval;
+
+done:
+	LOG_ERROR("d-cache invalidate failed");
+
+	return retval;
+}
+
+static int armv7a_l2x_cache_inval_virt(struct target *target, uint32_t virt,
+					uint32_t size)
+{
+	struct armv7a_common *armv7a = target_to_armv7a(target);
+	struct armv7a_l2x_cache *l2x_cache = (struct armv7a_l2x_cache *)
+		(armv7a->armv7a_mmu.armv7a_cache.l2_cache);
+	/* FIXME: different controllers have different linelen */
+	uint32_t i, linelen = 32;
+	int retval;
+
+	retval = arm7a_l2x_sanity_check(target);
+	if (retval)
+		return retval;
+
+	for (i = 0; i < size; i += linelen) {
+		uint32_t pa, offs = virt + i;
+
+		/* FIXME: use less verbose virt2phys? */
+		retval = target->type->virt2phys(target, offs, &pa);
+		if (retval != ERROR_OK)
+			goto done;
+
+		retval = target_write_phys_memory(target,
+				l2x_cache->base + L2X0_INV_LINE_PA,
+				4, 1, (uint8_t *)&pa);
+		if (retval != ERROR_OK)
+			goto done;
+	}
+	return retval;
+
+done:
+	LOG_ERROR("d-cache invalidate failed");
+
+	return retval;
+}
+
+static int armv7a_l2x_cache_clean_virt(struct target *target, uint32_t virt,
+					unsigned int size)
+{
+	struct armv7a_common *armv7a = target_to_armv7a(target);
+	struct armv7a_l2x_cache *l2x_cache = (struct armv7a_l2x_cache *)
+		(armv7a->armv7a_mmu.armv7a_cache.l2_cache);
+	/* FIXME: different controllers have different linelen */
+	uint32_t i, linelen = 32;
+	int retval;
+
+	retval = arm7a_l2x_sanity_check(target);
+	if (retval)
+		return retval;
+
+	for (i = 0; i < size; i += linelen) {
+		uint32_t pa, offs = virt + i;
+
+		/* FIXME: use less verbose virt2phys? */
+		retval = target->type->virt2phys(target, offs, &pa);
+		if (retval != ERROR_OK)
+			goto done;
+
+		retval = target_write_phys_memory(target,
+				l2x_cache->base + L2X0_CLEAN_LINE_PA,
+				4, 1, (uint8_t *)&pa);
+		if (retval != ERROR_OK)
+			goto done;
+	}
+	return retval;
+
+done:
+	LOG_ERROR("d-cache invalidate failed");
+
+	return retval;
+}
+
+static int arm7a_handle_l2x_cache_info_command(struct command_context *cmd_ctx,
+	struct armv7a_cache_common *armv7a_cache)
+{
+	struct armv7a_l2x_cache *l2x_cache = (struct armv7a_l2x_cache *)
+		(armv7a_cache->l2_cache);
+
+	if (armv7a_cache->ctype == -1) {
+		command_print(cmd_ctx, "cache not yet identified");
+		return ERROR_OK;
+	}
+
+	command_print(cmd_ctx,
+		      "L2 unified cache Base Address 0x%" PRIx32 ", %" PRId32 " ways",
+		      l2x_cache->base, l2x_cache->way);
+
+	return ERROR_OK;
+}
+
+static int armv7a_l2x_cache_init(struct target *target, uint32_t base, uint32_t way)
+{
+	struct armv7a_l2x_cache *l2x_cache;
+	struct target_list *head = target->head;
+	struct target *curr;
+
+	struct armv7a_common *armv7a = target_to_armv7a(target);
+	if (armv7a->armv7a_mmu.armv7a_cache.l2_cache) {
+		LOG_ERROR("L2 cache was already initialised\n");
+		return ERROR_FAIL;
+	}
+
+	l2x_cache = calloc(1, sizeof(struct armv7a_l2x_cache));
+	l2x_cache->base = base;
+	l2x_cache->way = way;
+	armv7a->armv7a_mmu.armv7a_cache.l2_cache = l2x_cache;
+
+	/*  initialize all targets in this cluster (smp target)
+	 *  l2 cache must be configured after smp declaration */
+	while (head != (struct target_list *)NULL) {
+		curr = head->target;
+		if (curr != target) {
+			armv7a = target_to_armv7a(curr);
+			if (armv7a->armv7a_mmu.armv7a_cache.l2_cache) {
+				LOG_ERROR("smp target : cache l2 already initialized\n");
+				return ERROR_FAIL;
+			}
+			armv7a->armv7a_mmu.armv7a_cache.l2_cache = l2x_cache;
+		}
+		head = head->next;
+	}
+	return ERROR_OK;
+}
+
+COMMAND_HANDLER(arm7a_l2x_cache_info_command)
+{
+	struct target *target = get_current_target(CMD_CTX);
+	struct armv7a_common *armv7a = target_to_armv7a(target);
+	int retval;
+
+	retval = arm7a_l2x_sanity_check(target);
+	if (retval)
+		return retval;
+
+	return arm7a_handle_l2x_cache_info_command(CMD_CTX,
+			&armv7a->armv7a_mmu.armv7a_cache);
+}
+
+COMMAND_HANDLER(arm7a_l2x_cache_flush_all_command)
+{
+	struct target *target = get_current_target(CMD_CTX);
+
+	return arm7a_l2x_flush_all_data(target);
+}
+
+COMMAND_HANDLER(arm7a_l2x_cache_flush_virt_cmd)
+{
+	struct target *target = get_current_target(CMD_CTX);
+	uint32_t virt, size;
+
+	if (CMD_ARGC == 0 || CMD_ARGC > 2)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	if (CMD_ARGC == 2)
+		COMMAND_PARSE_NUMBER(u32, CMD_ARGV[1], size);
+	else
+		size = 1;
+
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], virt);
+
+	return armv7a_l2x_cache_flush_virt(target, virt, size);
+}
+
+COMMAND_HANDLER(arm7a_l2x_cache_inval_virt_cmd)
+{
+	struct target *target = get_current_target(CMD_CTX);
+	uint32_t virt, size;
+
+	if (CMD_ARGC == 0 || CMD_ARGC > 2)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	if (CMD_ARGC == 2)
+		COMMAND_PARSE_NUMBER(u32, CMD_ARGV[1], size);
+	else
+		size = 1;
+
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], virt);
+
+	return armv7a_l2x_cache_inval_virt(target, virt, size);
+}
+
+COMMAND_HANDLER(arm7a_l2x_cache_clean_virt_cmd)
+{
+	struct target *target = get_current_target(CMD_CTX);
+	uint32_t virt, size;
+
+	if (CMD_ARGC == 0 || CMD_ARGC > 2)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	if (CMD_ARGC == 2)
+		COMMAND_PARSE_NUMBER(u32, CMD_ARGV[1], size);
+	else
+		size = 1;
+
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], virt);
+
+	return armv7a_l2x_cache_clean_virt(target, virt, size);
+}
+
+/* FIXME: should we configure way size? or controller type? */
+COMMAND_HANDLER(armv7a_l2x_cache_conf_cmd)
+{
+	struct target *target = get_current_target(CMD_CTX);
+	uint32_t base, way;
+
+	if (CMD_ARGC != 2)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	/* command_print(CMD_CTX, "%s %s", CMD_ARGV[0], CMD_ARGV[1]); */
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], base);
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[1], way);
+
+	/* AP address is in bits 31:24 of DP_SELECT */
+	return armv7a_l2x_cache_init(target, base, way);
+}
+
+static const struct command_registration arm7a_l2x_cache_commands[] = {
+	{
+		.name = "conf",
+		.handler = armv7a_l2x_cache_conf_cmd,
+		.mode = COMMAND_ANY,
+		.help = "configure l2x cache ",
+		.usage = "<base_addr> <number_of_way>",
+	},
+	{
+		.name = "info",
+		.handler = arm7a_l2x_cache_info_command,
+		.mode = COMMAND_ANY,
+		.help = "print cache realted information",
+		.usage = "",
+	},
+	{
+		.name = "flush_all",
+		.handler = arm7a_l2x_cache_flush_all_command,
+		.mode = COMMAND_ANY,
+		.help = "flush complete l2x cache",
+		.usage = "",
+	},
+	{
+		.name = "flush",
+		.handler = arm7a_l2x_cache_flush_virt_cmd,
+		.mode = COMMAND_ANY,
+		.help = "flush (clean and invalidate) l2x cache by virtual address offset and range size",
+		.usage = "<virt_addr> [size]",
+	},
+	{
+		.name = "inval",
+		.handler = arm7a_l2x_cache_inval_virt_cmd,
+		.mode = COMMAND_ANY,
+		.help = "invalidate l2x cache by virtual address offset and range size",
+		.usage = "<virt_addr> [size]",
+	},
+	{
+		.name = "clean",
+		.handler = arm7a_l2x_cache_clean_virt_cmd,
+		.mode = COMMAND_ANY,
+		.help = "clean l2x cache by virtual address address offset and range size",
+		.usage = "<virt_addr> [size]",
+	},
+	COMMAND_REGISTRATION_DONE
+};
+
+const struct command_registration arm7a_l2x_cache_command_handler[] = {
+	{
+		.name = "l2x",
+		.mode = COMMAND_ANY,
+		.help = "l2x cache command group",
+		.usage = "",
+		.chain = arm7a_l2x_cache_commands,
+	},
+	COMMAND_REGISTRATION_DONE
+};

src/target/Makefile.am
@@ -91,6 +91,8 @@ ARM_DEBUG_SRC = \
 	arm_simulator.c \
 	arm_semihosting.c \
 	arm_adi_v5.c \
+	armv7a_cache.c \
+	armv7a_cache_l2x.c \
 	adi_v5_jtag.c \
 	adi_v5_swd.c \
 	embeddedice.c \
@@ -136,6 +138,8 @@ noinst_HEADERS = \
 	arm_dpm.h \
 	arm_jtag.h \
 	arm_adi_v5.h \
+	armv7a_cache.h \
+	armv7a_cache_l2x.h \
 	arm_disassembler.h \
 	arm_opcodes.h \
 	arm_simulator.h \

src/target/armv7a.c
@@ -647,15 +647,28 @@ int armv7a_identify_cache(struct target *target)
 	int retval = ERROR_FAIL;
 	struct armv7a_common *armv7a = target_to_armv7a(target);
 	struct arm_dpm *dpm = armv7a->arm.dpm;
-	uint32_t cache_selected, clidr;
+	uint32_t cache_selected, clidr, ctr;
 	uint32_t cache_i_reg, cache_d_reg;
 	struct armv7a_cache_common *cache = &(armv7a->armv7a_mmu.armv7a_cache);
 	if (!armv7a->is_armv7r)
 		armv7a_read_ttbcr(target);
 	retval = dpm->prepare(dpm);
-
 	if (retval != ERROR_OK)
 		goto done;
+
+	/* retrieve CTR
+	 * mrc p15, 0, r0, c0, c0, 1		@ read ctr */
+	retval = dpm->instr_read_data_r0(dpm,
+			ARMV4_5_MRC(15, 0, 0, 0, 0, 1),
+			&ctr);
+	if (retval != ERROR_OK)
+		goto done;
+
+	cache->iminline = 4UL << (ctr & 0xf);
+	cache->dminline = 4UL << ((ctr & 0xf0000) >> 16);
+	LOG_DEBUG("ctr %" PRIx32 " ctr.iminline %" PRId32 " ctr.dminline %" PRId32,
+		 ctr, cache->iminline, cache->dminline);
+
 	/*  retrieve CLIDR
 	 *  mrc	p15, 1, r0, c0, c0, 1		@ read clidr */
 	retval = dpm->instr_read_data_r0(dpm,
@@ -806,6 +819,7 @@ int armv7a_init_arch_info(struct target *target, struct armv7a_common *armv7a)
 	armv7a->armv7a_mmu.armv7a_cache.ctype = -1;
 	armv7a->armv7a_mmu.armv7a_cache.flush_all_data_cache = NULL;
 	armv7a->armv7a_mmu.armv7a_cache.display_cache_info = NULL;
+	armv7a->armv7a_mmu.armv7a_cache.auto_cache_enabled = 1;
 	return ERROR_OK;
 }
 
@@ -869,13 +883,15 @@ const struct command_registration l2x_cache_command_handlers[] = {
 	COMMAND_REGISTRATION_DONE
 };
 
-
 const struct command_registration armv7a_command_handlers[] = {
 	{
 		.chain = dap_command_handlers,
 	},
 	{
 		.chain = l2x_cache_command_handlers,
+	},
+	{
+		.chain = arm7a_cache_command_handlers,
 	},
 	COMMAND_REGISTRATION_DONE
 };

src/target/armv7a.h
@@ -21,6 +21,7 @@
 #define ARMV7A_H
 
 #include "arm_adi_v5.h"
+#include "armv7a_cache.h"
 #include "arm.h"
 #include "armv4_5_mmu.h"
 #include "armv4_5_cache.h"
@@ -66,8 +67,12 @@ struct armv7a_cache_common {
 	int ctype;
 	struct armv7a_cachesize d_u_size;	/* data cache */
 	struct armv7a_cachesize i_size;		/* instruction cache */
+	uint32_t dminline;			/* minimum d-cache linelen */
+	uint32_t iminline;			/* minimum i-cache linelen */
 	int i_cache_enabled;
 	int d_u_cache_enabled;
+	int auto_cache_enabled;			/* openocd automatic
+						 * cache handling */
 	/* l2 external unified cache if some */
 	void *l2_cache;
 	int (*flush_all_data_cache)(struct target *target);

commit b44eea763c628887423d783ba03710da24492df2

target: Introduce target_write_phys_u32() for ARMv7-A cache
Add a wrapper target_write_phys_u32() for handling 32-bit writes with
target_write_phys_memory() in an endian-safe way.

Use this wrapper to fix incorrect usages of target_write_phys_memory()
in armv7a_l2c_flash_all_data(), armv7a_l2x_cache_flush_virt(),
armv7a_l2x_cache_inval_virt() and armv7a_l2x_cache_clean_virt().

Change-Id: Id259b58f6342af2c456e8a291ff12f427174b4cd
Fixes: cd440bd ("add armv7a_cache handlers")
Signed-off-by: Andreas Färber <afaerber@suse.de>

src/target/armv7a_cache_l2x.c
@@ -63,9 +63,9 @@ int arm7a_l2x_flush_all_data(struct target *target)
 
 	l2_way_val = (1 << l2x_cache->way) - 1;
 
-	return target_write_phys_memory(target,
+	return target_write_phys_u32(target,
 			l2x_cache->base + L2X0_CLEAN_INV_WAY,
-			4, 1, (uint8_t *)&l2_way_val);
+			l2_way_val);
 }
 
 int armv7a_l2x_cache_flush_virt(struct target *target, uint32_t virt,
@@ -90,9 +90,8 @@ int armv7a_l2x_cache_flush_virt(struct target *target, uint32_t virt,
 		if (retval != ERROR_OK)
 			goto done;
 
-		retval = target_write_phys_memory(target,
-				l2x_cache->base + L2X0_CLEAN_INV_LINE_PA,
-				4, 1, (uint8_t *)&pa);
+		retval = target_write_phys_u32(target,
+				l2x_cache->base + L2X0_CLEAN_INV_LINE_PA, pa);
 		if (retval != ERROR_OK)
 			goto done;
 	}
@@ -126,9 +125,8 @@ static int armv7a_l2x_cache_inval_virt(struct target *target, uint32_t virt,
 		if (retval != ERROR_OK)
 			goto done;
 
-		retval = target_write_phys_memory(target,
-				l2x_cache->base + L2X0_INV_LINE_PA,
-				4, 1, (uint8_t *)&pa);
+		retval = target_write_phys_u32(target,
+				l2x_cache->base + L2X0_INV_LINE_PA, pa);
 		if (retval != ERROR_OK)
 			goto done;
 	}
@@ -162,9 +160,8 @@ static int armv7a_l2x_cache_clean_virt(struct target *target, uint32_t virt,
 		if (retval != ERROR_OK)
 			goto done;
 
-		retval = target_write_phys_memory(target,
-				l2x_cache->base + L2X0_CLEAN_LINE_PA,
-				4, 1, (uint8_t *)&pa);
+		retval = target_write_phys_u32(target,
+				l2x_cache->base + L2X0_CLEAN_LINE_PA, pa);
 		if (retval != ERROR_OK)
 			goto done;
 	}

src/target/target.c
@@ -2369,6 +2369,27 @@ int target_write_u8(struct target *target, uint32_t address, uint8_t value)
 	return retval;
 }
 
+int target_write_phys_u32(struct target *target, uint32_t address, uint32_t value)
+{
+	int retval;
+	uint8_t value_buf[4];
+	if (!target_was_examined(target)) {
+		LOG_ERROR("Target not examined yet");
+		return ERROR_FAIL;
+	}
+
+	LOG_DEBUG("address: 0x%8.8" PRIx32 ", value: 0x%8.8" PRIx32 "",
+			  address,
+			  value);
+
+	target_buffer_set_u32(target, value_buf, value);
+	retval = target_write_phys_memory(target, address, 4, 1, value_buf);
+	if (retval != ERROR_OK)
+		LOG_DEBUG("failed: %i", retval);
+
+	return retval;
+}
+
 static int find_target(struct command_context *cmd_ctx, const char *name)
 {
 	struct target *target = get_target(name);

src/target/target.h
@@ -664,6 +664,8 @@ int target_write_u32(struct target *target, uint32_t address, uint32_t value);
 int target_write_u16(struct target *target, uint32_t address, uint16_t value);
 int target_write_u8(struct target *target, uint32_t address, uint8_t value);
 
+int target_write_phys_u32(struct target *target, uint32_t address, uint32_t value);
+
 /* Issues USER() statements with target state information */
 int target_arch_state(struct target *target);
 