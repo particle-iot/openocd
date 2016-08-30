fc964ff24abd05a99513b985da3f1d7d4c5a4634
Austin Phillips <austin_phillips@hotmail.com>
2016-08-31 08:06:32 +1000

    libusb1_common: fix non-ASCII characters in serial numbers
    libusb1's libusb_get_string_descriptor_ascii() replaces non ASCII
    characters with '?' (0x3f).
    Non ASCII characters in USB serials are found in the wild on
    ST-Link/V2 dongles and STM32 Discovery boards,
    which have serials like "Q\377j\006I\207PS(H\t\207".
    This patch uses libusb_get_string_descriptor() to read the non ASCII
    characters properly.
    
    Change-Id: I5fd7b626342dba5811fe81ab581ec22584a5e6d5
    Signed-off-by: Scott Tsai <scottt.tw@gmail.com>
    Signed-off-by: Austin Phillips <austin_phillips@hotmail.com>

--- src/jtag/drivers/stlink_usb.c
+++ src/jtag/drivers/stlink_usb.c
@@ -36,6 +36,7 @@
 #include <target/cortex_m.h>
 
 #include "libusb_common.h"
+#include "unicode.h"
 
 #define ENDPOINT_IN  0x80
 #define ENDPOINT_OUT 0x00
@@ -1652,11 +1653,13 @@ static int stlink_usb_open(struct hl_interface_param_s *param, void **fd)
 
 	const uint16_t vids[] = { param->vid, 0 };
 	const uint16_t pids[] = { param->pid, 0 };
-	const char *serial = param->serial;
+	char serial_text[256];
+
+	utf8_to_text((const uint8_t *)param->serial, serial_text, sizeof(serial_text));
 
 	LOG_DEBUG("transport: %d vid: 0x%04x pid: 0x%04x serial: %s",
 			param->transport, param->vid, param->pid,
-			param->serial ? param->serial : "");
+			serial_text);
 
 	/*
 	  On certain host USB configurations(e.g. MacBook Air)
@@ -1668,8 +1671,8 @@ static int stlink_usb_open(struct hl_interface_param_s *param, void **fd)
 	  in order to become operational.
 	 */
 	do {
-		if (jtag_libusb_open(vids, pids, serial, &h->fd) != ERROR_OK) {
-			LOG_ERROR("open failed");
+		if (jtag_libusb_open(vids, pids, param->serial, &h->fd) != ERROR_OK) {
+			LOG_ERROR("open failed (no matching adapter found)");
 			goto error_open;
 		}
 
