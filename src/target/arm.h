7fcba2f70ceedf61e0c97d99b727d7495671aec2
Andreas Fritiofson <andreas.fritiofson@gmail.com>
2016-12-28 23:43:53 +0100

    arm: semihosting: set command line arguments
    Add "arm semihosting_cmdline [argv0 argv1 ...]" for setting the
    command line arguments for the debuggee.
    
    [andreas.fritiofson@gmail.com]: Dynamic allocation, empty default
    
    Change-Id: I831ddd161d602f251940e29608a154e9590fdee1
    Signed-off-by: Christian Groessler <chris@groessler.org>
    Signed-off-by: Andreas Fritiofson <andreas.fritiofson@gmail.com>

--- src/target/arm.h
+++ src/target/arm.h
@@ -147,6 +147,9 @@ struct arm {
 
 	int (*setup_semihosting)(struct target *target, int enable);
 
+	/** Semihosting command line. */
+	char *semihosting_cmdline;
+
 	/** Backpointer to the target. */
 	struct target *target;
 
