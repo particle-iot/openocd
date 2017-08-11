6fb9def3d732a4e53bbc0d79d1dffdb0d307b7e2
Fabio Utzig <utzig@apache.org>
2017-08-11 13:05:56 -0300

    Fix gcc7 errors for fall through cases in switch
    GCC 7 by default fails when it detects a case statement that has no
    associated break. This patch adds comments which gcc will use as
    hint that the case should really fall through and enables openocd to
    be built without adding a `Wno-error` flag.
    
    Change-Id: Ia03339a19d7fd2a74bd1dfcf740b1121d7d67c71
    Signed-off-by: Fabio Utzig <utzig@apache.org>

--- src/target/target.c
+++ src/target/target.c
@@ -3684,6 +3684,7 @@ COMMAND_HANDLER(handle_bp_command)
 				addr = 0;
 				return handle_bp_command_set(CMD_CTX, addr, asid, length, hw);
 			}
+			/* FALL THROUGH */
 
 		case 4:
 			hw = BKPT_HARD;
