d23052c703fbbdda70281d1da949aac32a9338bb
Tomas Vanek <vanekt@fbl.cz>
2017-11-02 13:01:45 +0100

    target/cortex_a: convert assert_reset to reset_prepare_trigger [WIP]
    Also contains a modified imx6.cfg for testing
    
    Change-Id: I77d4cabbef5801578b97eff73a4ef2eb761e382e
    Signed-off-by: Tomas Vanek <vanekt@fbl.cz>

--- src/target/cortex_a.c
+++ src/target/cortex_a.c
@@ -1852,6 +1852,7 @@ static int cortex_a_remove_breakpoint(struct target *target, struct breakpoint *
  * Cortex-A Reset functions
  */
 
+#if 0
 static int cortex_a_assert_reset(struct target *target)
 {
 	struct armv7a_common *armv7a = target_to_armv7a(target);
@@ -1922,6 +1923,7 @@ static int cortex_a_deassert_reset(struct target *target)
 
 	return ERROR_OK;
 }
+#endif
 
 static int cortex_a_set_dcc_mode(struct target *target, uint32_t mode, uint32_t *dscr)
 {
@@ -3440,8 +3442,10 @@ struct target_type cortexa_target = {
 	.resume = cortex_a_resume,
 	.step = cortex_a_step,
 
-	.assert_reset = cortex_a_assert_reset,
-	.deassert_reset = cortex_a_deassert_reset,
+	.reset_clear_internal_state = armv7a_reset_clear_internal_state,
+/*	.reset_prepare_trigger = cortex_a_reset_prepare_trigger,*/
+/*	.assert_reset = cortex_a_assert_reset,*/
+/*	.deassert_reset = cortex_a_deassert_reset,*/
 
 	/* REVISIT allow exporting VFP3 registers ... */
 	.get_gdb_reg_list = arm_get_gdb_reg_list,
@@ -3528,8 +3532,10 @@ struct target_type cortexr4_target = {
 	.resume = cortex_a_resume,
 	.step = cortex_a_step,
 
-	.assert_reset = cortex_a_assert_reset,
-	.deassert_reset = cortex_a_deassert_reset,
+	.reset_clear_internal_state = armv7a_reset_clear_internal_state,
+/*	.reset_prepare_trigger = cortex_a_reset_prepare_trigger,*/
+/*	.assert_reset = cortex_a_assert_reset,*/
+/*	.deassert_reset = cortex_a_deassert_reset,*/
 
 	/* REVISIT allow exporting VFP3 registers ... */
 	.get_gdb_reg_list = arm_get_gdb_reg_list,
