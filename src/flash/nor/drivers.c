c706e874a8f1afbb5ebac4dcd1a9de020270b1fd
Edward Fewell <efewell@ti.com>
2017-12-28 17:38:21 -0600

    flash/nor: Add support for TI CC3220SF internal flash
    Added cc3220sf flash driver to support the TI CC3220SF microcontrollers.
    Implemented flash driver to support the internal flash of the CC3220SF. The
    implementation does not support the serial flash of the CC32xx family that
    requires connection over UART, and not via JTAG/SWD debug. Added config files
    for both CC32xx devices (no flash) and CC3220SF (with flash).
    
    Updated to implement comments from code review.
    
    Additional updates to handle remaining comments from code review.
    
    Change-Id: I58fc1478d07238d39c7ef02339f1097a91668c47
    Signed-off-by: Edward Fewell <efewell@ti.com>

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -31,6 +31,7 @@ extern struct flash_driver at91samd_flash;
 extern struct flash_driver ath79_flash;
 extern struct flash_driver atsamv_flash;
 extern struct flash_driver avr_flash;
+extern struct flash_driver cc3220sf_flash;
 extern struct flash_driver cfi_flash;
 extern struct flash_driver dsp5680xx_flash;
 extern struct flash_driver efm32_flash;
@@ -86,6 +87,7 @@ static struct flash_driver *flash_drivers[] = {
 	&ath79_flash,
 	&atsamv_flash,
 	&avr_flash,
+	&cc3220sf_flash,
 	&cfi_flash,
 	&dsp5680xx_flash,
 	&efm32_flash,
