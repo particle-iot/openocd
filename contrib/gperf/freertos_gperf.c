199352ce6daf8a976a94b55bb0c2a68653c3c9e5
Vlad Ivanov <vlad@ivanov.email>
2018-04-06 05:01:23 -0400

    Initial support for dynamic RTOS configurations
    Change-Id: Ia38e198dafba7ec9226470566f81f11b02004c4a
    Signed-off-by: Vlad Ivanov <vlad@ivanov.email>

---
+++ contrib/gperf/freertos_gperf.c
@@ -0,0 +1,144 @@
+/* ANSI-C code produced by gperf version 3.1 */
+/* Command-line: gperf -L ANSI-C -t freertos.gperf  */
+/* Computed positions: -k'8' */
+
+#if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
+      && ('%' == 37) && ('&' == 38) && ('\'' == 39) && ('(' == 40) \
+      && (')' == 41) && ('*' == 42) && ('+' == 43) && (',' == 44) \
+      && ('-' == 45) && ('.' == 46) && ('/' == 47) && ('0' == 48) \
+      && ('1' == 49) && ('2' == 50) && ('3' == 51) && ('4' == 52) \
+      && ('5' == 53) && ('6' == 54) && ('7' == 55) && ('8' == 56) \
+      && ('9' == 57) && (':' == 58) && (';' == 59) && ('<' == 60) \
+      && ('=' == 61) && ('>' == 62) && ('?' == 63) && ('A' == 65) \
+      && ('B' == 66) && ('C' == 67) && ('D' == 68) && ('E' == 69) \
+      && ('F' == 70) && ('G' == 71) && ('H' == 72) && ('I' == 73) \
+      && ('J' == 74) && ('K' == 75) && ('L' == 76) && ('M' == 77) \
+      && ('N' == 78) && ('O' == 79) && ('P' == 80) && ('Q' == 81) \
+      && ('R' == 82) && ('S' == 83) && ('T' == 84) && ('U' == 85) \
+      && ('V' == 86) && ('W' == 87) && ('X' == 88) && ('Y' == 89) \
+      && ('Z' == 90) && ('[' == 91) && ('\\' == 92) && (']' == 93) \
+      && ('^' == 94) && ('_' == 95) && ('a' == 97) && ('b' == 98) \
+      && ('c' == 99) && ('d' == 100) && ('e' == 101) && ('f' == 102) \
+      && ('g' == 103) && ('h' == 104) && ('i' == 105) && ('j' == 106) \
+      && ('k' == 107) && ('l' == 108) && ('m' == 109) && ('n' == 110) \
+      && ('o' == 111) && ('p' == 112) && ('q' == 113) && ('r' == 114) \
+      && ('s' == 115) && ('t' == 116) && ('u' == 117) && ('v' == 118) \
+      && ('w' == 119) && ('x' == 120) && ('y' == 121) && ('z' == 122) \
+      && ('{' == 123) && ('|' == 124) && ('}' == 125) && ('~' == 126))
+/* The character set is not based on ISO-646.  */
+#error "gperf generated tables don't work with this execution character set. Please report a bug to <bug-gperf@gnu.org>."
+#endif
+
+#line 2 "freertos.gperf"
+
+typedef enum {
+	freertos_target_name = 0,
+	freertos_thread_count_width,
+	freertos_pointer_width,
+	freertos_list_next_offset,
+	freertos_list_width,
+	freertos_list_elem_next_offset,
+	freertos_list_elem_content_offset,
+	freertos_thread_stack_offset,
+	freertos_thread_name_offset,
+	freertos_option_count,
+	freertos_invalid = -1,
+} freertos_option_t;
+#line 17 "freertos.gperf"
+struct freertos_lookup_entry { char * name; freertos_option_t option; };
+
+#define TOTAL_KEYWORDS 9
+#define MIN_WORD_LENGTH 10
+#define MAX_WORD_LENGTH 24
+#define MIN_HASH_VALUE 10
+#define MAX_HASH_VALUE 24
+/* maximum key range = 15, duplicates = 0 */
+
+#ifdef __GNUC__
+__inline
+#else
+#ifdef __cplusplus
+inline
+#endif
+#endif
+static unsigned int
+hash (register const char *str, register size_t len)
+{
+  static unsigned char asso_values[] =
+    {
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25,  0, 25, 25, 25,  5,
+       0,  0, 25, 25, 25, 25, 25, 25, 25, 25,
+       0, 25, 25, 25, 25,  0, 25, 25, 25, 25,
+       0, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+      25, 25, 25, 25, 25, 25
+    };
+  return len + asso_values[(unsigned char)str[7]];
+}
+
+struct freertos_lookup_entry *
+freertos_in_word_set (register const char *str, register size_t len)
+{
+  static struct freertos_lookup_entry wordlist[] =
+    {
+      {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""},
+      {""},
+#line 23 "freertos.gperf"
+      {"list_width",                 freertos_list_width},
+#line 19 "freertos.gperf"
+      {"target_name",                freertos_target_name},
+      {""},
+#line 21 "freertos.gperf"
+      {"pointer_width",              freertos_pointer_width},
+      {""}, {""},
+#line 22 "freertos.gperf"
+      {"list_next_offset",           freertos_list_next_offset},
+      {""},
+#line 27 "freertos.gperf"
+      {"thread_name_offset",         freertos_thread_name_offset},
+#line 26 "freertos.gperf"
+      {"thread_stack_offset",        freertos_thread_stack_offset},
+      {""},
+#line 24 "freertos.gperf"
+      {"list_elem_next_offset",      freertos_list_elem_next_offset},
+      {""},
+#line 20 "freertos.gperf"
+      {"thread_count_width",         freertos_thread_count_width},
+#line 25 "freertos.gperf"
+      {"list_elem_content_offset",   freertos_list_elem_content_offset}
+    };
+
+  if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)
+    {
+      register unsigned int key = hash (str, len);
+
+      if (key <= MAX_HASH_VALUE)
+        {
+          register const char *s = wordlist[key].name;
+
+          if (*str == *s && !strcmp (str + 1, s + 1))
+            return &wordlist[key];
+        }
+    }
+  return 0;
+}
