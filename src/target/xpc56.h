d64302a039cdedb652924664a3fb02bceb542282
Antoine Calando <acalando@free.fr>
2018-05-31 15:24:12 +0200

    DRAFT - target: add support for powerpc based microcontrollers
    this patch is adding support for new architecture,
    powerpc e200 core found in mpc56xx/spc56xx from
    nxp/stmicro
    
    Change-Id: I3bc9bdb92e87accba24224fad8383767e3cf03fa
    Signed-off-by: Jiri Kastner <cz172638@gmail.com>

---
+++ src/target/xpc56.h
@@ -0,0 +1,34 @@
+/**************************************************************************
+*   Copyright (C) 2018 by Antoine Calando                                 *
+*   acalando@free.fr                                                      *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+**************************************************************************/
+
+/* HELPERS */
+
+#define BIT(x) (1<<(x))
+#define CHECK_RETVAL(action)			\
+	do {					\
+		int __retval = (action);	\
+		if (__retval != ERROR_OK) {	\
+			LOG_DEBUG("error while calling \"%s\"",	\
+				# action);     \
+			return __retval;	\
+		}				\
+	} while (0)
+
+
+uint32_t xpc56_read_u32(struct target *target, target_addr_t address);
+void xpc56_write_u32(struct target *target, target_addr_t address, uint32_t value);
