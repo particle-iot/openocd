a47b95961cdbc2bbabda454dec0650cf0b6b2acc
Oleksij Rempel <o.rempel@pengutronix.de>
2018-07-05 12:08:13 +0200

    drivers: USB Blaster II: claim interface before using it
    If not, multiple instances of OOCD can concurrently use it.
    
    Change-Id: I48cc9d90521d1dcc7720c6e8bec74f45972d16f7
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>

--- src/jtag/drivers/usb_blaster/ublast2_access_libusb.c
+++ src/jtag/drivers/usb_blaster/ublast2_access_libusb.c
@@ -124,6 +124,11 @@ static int load_usb_blaster_firmware(struct jtag_libusb_device_handle *libusb_de
 		return ERROR_FAIL;
 	}
 
+	if (jtag_libusb_claim_interface(libusb_dev, 0)) {
+		LOG_ERROR("unable to claim interface");
+		return ERROR_JTAG_INIT_FAILED;
+	}
+
 	ublast2_firmware_image.base_address = 0;
 	ublast2_firmware_image.base_address_set = 0;
 
@@ -218,6 +223,12 @@ static int ublast2_libusb_init(struct ublast_lowlevel *low)
 		}
 	}
 
+	if (jtag_libusb_claim_interface(low->libusb_dev, 0)) {
+		LOG_ERROR("unable to claim interface");
+		jtag_libusb_close(low->libusb_dev);
+		return ERROR_JTAG_INIT_FAILED;
+	}
+
 	char buffer[5];
 	jtag_libusb_control_transfer(low->libusb_dev,
 				     LIBUSB_REQUEST_TYPE_VENDOR | \
@@ -236,6 +247,9 @@ static int ublast2_libusb_init(struct ublast_lowlevel *low)
 
 static int ublast2_libusb_quit(struct ublast_lowlevel *low)
 {
+	if (jtag_libusb_release_interface(low->libusb_dev, 0))
+		LOG_ERROR("usb release interface failed");
+
 	jtag_libusb_close(low->libusb_dev);
 	return ERROR_OK;
 };
