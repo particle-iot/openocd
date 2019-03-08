d5e5751e8139cc39309c84e5b8c47b7b0ab4794f
hazel zhang <hazel.zhang@unisoc.com>
2019-03-08 14:06:25 +0800

    flash/nor: Add support for UWP5661 flash
    Add support for UWP5661 flash.
    The flash driver supports flash chip W25Q32DW and W25Q32JW.
    It probe UWP5661 chip and configure its flash automatically,
    erase chip and download bin file in the current directory.
    
    Change-Id: I4524bed5a0b99f51916a500cee11276170b20721
    Signed-off-by: hazel zhang <hazel.zhang@unisoc.com>

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -78,6 +78,7 @@ extern struct flash_driver str7x_flash;
 extern struct flash_driver str9x_flash;
 extern struct flash_driver str9xpec_flash;
 extern struct flash_driver tms470_flash;
+extern struct flash_driver uwp5661_flash;
 extern struct flash_driver virtual_flash;
 extern struct flash_driver w600_flash;
 extern struct flash_driver xcf_flash;
@@ -147,6 +148,7 @@ static struct flash_driver *flash_drivers[] = {
 	&str9x_flash,
 	&str9xpec_flash,
 	&tms470_flash,
+	&uwp5661_flash,
 	&virtual_flash,
 	&xcf_flash,
 	&xmc1xxx_flash,
