particle-iot/openocd

commit 89fa493a3bc34d22eeca06fa4e78523ac3b766a8

remove unknown handler
Updates command registration to provide top-level handlers for all
commands, rather than falling back onto the 'unknown' command. Instead,
that same handler is registered for placeholders, providing the same
functionality under the root verb command name instead.  This permits
users to implement their own 'unknown' function, and it resolves some
mind-bending breakage related to function object lookup while recursing.

Changes 'ocd_bounce' to call 'ocd_command' and 'ocd_help' from the
wrapper directly, rather than bouncing through their wrappers. This
prevents endless recursion caused by the above changes, whereby the
'command' wrapper's type check would blow the stack to hell and gone.

src/helper/command.c
@@ -296,37 +296,32 @@ static void command_free(struct command *c)
 	free(c);
 }
 
+static int command_unknown(Jim_Interp *interp, int argc, Jim_Obj *const *argv);
+
 static int register_command_handler(struct command *c)
 {
-	int retval = -ENOMEM;
-	const char *full_name = command_name(c, '_');
-	if (NULL == full_name)
+	const char *ocd_name = alloc_printf("ocd_%s", c->name);
+	if (NULL == ocd_name)
+		return JIM_ERR;
+
+	LOG_DEBUG("registering '%s'...", ocd_name);
+
+	Jim_CmdProc func = c->handler ? &script_command : &command_unknown;
+	int retval = Jim_CreateCommand(interp, ocd_name, func, c, NULL);
+	free((void *)ocd_name);
+	if (JIM_OK != retval)
 		return retval;
-
-	if (NULL != c->handler)
-	{
-		const char *ocd_name = alloc_printf("ocd_%s", full_name);
-		if (NULL == full_name)
-			goto free_full_name;
-
-		Jim_CreateCommand(interp, ocd_name, script_command, c, NULL);
-		free((void *)ocd_name);
-	}
 
 	/* we now need to add an overrideable proc */
 	const char *override_name = alloc_printf(
 			"proc %s {args} {eval ocd_bouncer %s $args}",
-			full_name, full_name);
+			c->name, c->name);
 	if (NULL == override_name)
-		goto free_full_name;
+		return JIM_ERR;
 
-	Jim_Eval_Named(interp, override_name, __THIS__FILE__, __LINE__);
+	retval = Jim_Eval_Named(interp, override_name, __FILE__, __LINE__);
 	free((void *)override_name);
 
-	retval = ERROR_OK;
-
-free_full_name:
-	free((void *)full_name);
 	return retval;
 }
 
@@ -350,19 +345,20 @@ struct command* register_command(struct command_context *context,
 	if (NULL == c)
 		return NULL;
 
-	if (NULL != c->handler)
-	{
-		int retval = register_command_handler(command_root(c));
-		if (ERROR_OK != retval)
-		{
-			unregister_command(context, parent, name);
-			return NULL;
-		}
-	}
-
+	int retval = ERROR_OK;
 	if (NULL != cr->jim_handler && NULL == parent)
-		Jim_CreateCommand(interp, cr->name, cr->jim_handler, cr->jim_handler_data, NULL);
+	{
+		retval = Jim_CreateCommand(interp, cr->name,
+				cr->jim_handler, cr->jim_handler_data, NULL);
+	}
+	else if (NULL != cr->handler || NULL != parent)
+		retval = register_command_handler(command_root(c));
 
+	if (ERROR_OK != retval)
+	{
+		unregister_command(context, parent, name);
+		c = NULL;
+	}
 	return c;
 }
 
@@ -903,15 +899,22 @@ static int command_unknown_find(unsigned argc, Jim_Obj *const *argv,
 static int command_unknown(Jim_Interp *interp, int argc, Jim_Obj *const *argv)
 {
 	const char *cmd_name = Jim_GetString(argv[0], NULL);
-	script_debug(interp, cmd_name, argc - 1, argv + 1);
+	if (strcmp(cmd_name, "unknown") == 0)
+	{
+		if (argc == 1)
+			return JIM_OK;
+		argc--;
+		argv++;
+	}
+	script_debug(interp, cmd_name, argc, argv);
 
 	struct command_context *cmd_ctx = current_command_context();
 	struct command *c = cmd_ctx->commands;
-	int remaining = command_unknown_find(argc - 1, argv + 1, c, &c, true);
+	int remaining = command_unknown_find(argc, argv, c, &c, true);
 	// if nothing could be consumed, then it's really an unknown command
-	if (remaining == argc - 1)
+	if (remaining == argc)
 	{
-		const char *cmd = Jim_GetString(argv[1], NULL);
+		const char *cmd = Jim_GetString(argv[0], NULL);
 		LOG_ERROR("Unknown command:\n  %s", cmd);
 		return JIM_OK;
 	}
@@ -1196,7 +1199,6 @@ struct command_context* command_init(const char *startup_tcl)
 	Jim_SetGlobalVariableStr(interp, "ocd_HOSTOS",
 			Jim_NewStringObj(interp, HostOs , strlen(HostOs)));
 
-	Jim_CreateCommand(interp, "unknown", &command_unknown, NULL, NULL);
 	Jim_CreateCommand(interp, "ocd_find", jim_find, NULL, NULL);
 	Jim_CreateCommand(interp, "echo", jim_echo, NULL, NULL);
 	Jim_CreateCommand(interp, "capture", jim_capture, NULL, NULL);

src/helper/startup.tcl
@@ -15,7 +15,7 @@ proc exit {} {
 # to discard 'handler' command output,
 proc ocd_bouncer {name args} {
 	set cmd [format "ocd_%s" $name]
-	set type [eval command type $cmd $args]
+	set type [eval ocd_command type $cmd $args]
 	if {$type == "native"} {
 		return [eval $cmd $args]
 	} else {if {$type == "simple"} {
@@ -25,7 +25,7 @@ proc ocd_bouncer {name args} {
 			set errmsg "Command handler execution failed"
 		}
 	} else {if {$type == "group"} {
-		catch {eval help $name $args}
+		catch {eval ocd_help $name $args}
 		set errmsg [format "%s: command requires more arguments" \
 			[concat $name " " $args]]
 	} else {

commit 2c98d20e96c4c4b42346b9ba65b7b860c7f84a4b

helper/command: remove dead code in command_unknown()
Commit 89fa493a3bc34d22eeca06fa4e78523ac3b766a8 removes the top
level command "unknown" and adds in command_unknown() some unclear
code that should detect a user-implemented "unknown" command.
But, all the commands that trigger the execution of the function
command_unknown() are registered in register_command_handler() and
have the command name prefixed with "ocd_".
Due to such "ocd_" prefix in cmd_name, the condition
	if (strcmp(cmd_name, "unknown") == 0) { ... }
is always false.
Nobody complained for almost 10 years so we can proceed to remove
this dead code, before merging any change that obsoletes the
prefix "ocd_" and potentially makes the condition true.

Remove the dead code guarded by the condition above.

Change-Id: Icf11a956bb2d68fc84e6eb5779edf8e35db8fa53
Fixes: 89fa493a3bc3 ("remove unknown handler")
Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>

src/helper/command.c
@@ -999,12 +999,6 @@ static int command_unknown_find(unsigned argc, Jim_Obj *const *argv,
 static int command_unknown(Jim_Interp *interp, int argc, Jim_Obj *const *argv)
 {
 	const char *cmd_name = Jim_GetString(argv[0], NULL);
-	if (strcmp(cmd_name, "unknown") == 0) {
-		if (argc == 1)
-			return JIM_OK;
-		argc--;
-		argv++;
-	}
 	script_debug(interp, cmd_name, argc, argv);
 
 	struct command_context *cmd_ctx = current_command_context(interp);