b2d7dae4fbac41d7e16dded051e63ce6d24fee23
Marek Vasut <marek.vasut@gmail.com>
2019-04-13 22:44:45 +0200

    flash/nor: Export various functions from the CFI core
    Export various functions needed by other driver, specifically the
    upcoming Renesas RPC HF driver. No functional change.
    
    Change-Id: I551258979a7221288fb4f4382f857db5cfe0b0de
    Signed-off-by: Marek Vasut <marek.vasut@gmail.com>

--- src/flash/nor/cfi.h
+++ src/flash/nor/cfi.h
@@ -155,6 +155,27 @@ struct cfi_fixup {
 	const void *param;
 };
 
+int cfi_erase(struct flash_bank *bank, int first, int last);
+int cfi_protect(struct flash_bank *bank, int set, int first, int last);
+int cfi_probe(struct flash_bank *bank);
+int cfi_auto_probe(struct flash_bank *bank);
+int cfi_protect_check(struct flash_bank *bank);
+int get_cfi_info(struct flash_bank *bank, char *buf, int buf_size);
+int cfi_flash_bank_cmd(struct flash_bank *bank, unsigned int argc, const char **argv);
+
+uint32_t flash_address(struct flash_bank *bank, int sector, uint32_t offset);
+int cfi_spansion_unlock_seq(struct flash_bank *bank);
+int cfi_send_command(struct flash_bank *bank, uint8_t cmd, uint32_t address);
+int cfi_write_word(struct flash_bank *bank, uint8_t *word, uint32_t address);
+int cfi_spansion_wait_status_busy(struct flash_bank *bank, int timeout);
+int cfi_reset(struct flash_bank *bank);
+
+int cfi_target_write_memory(struct flash_bank *bank, target_addr_t addr,
+			    uint32_t size, uint32_t count,
+			    const uint8_t *buffer);
+int cfi_target_read_memory(struct flash_bank *bank, target_addr_t addr,
+			   uint32_t size, uint32_t count, uint8_t *buffer);
+
 #define CFI_MFR_AMD		0x0001
 #define CFI_MFR_FUJITSU	0x0004
 #define CFI_MFR_ATMEL	0x001F
@@ -166,5 +187,8 @@ struct cfi_fixup {
 
 #define CFI_MFR_ANY		0xffff
 #define CFI_ID_ANY		0xffff
+
+#define CFI_MAX_BUS_WIDTH       4
+#define CFI_MAX_CHIP_WIDTH      4
 
 #endif /* OPENOCD_FLASH_NOR_CFI_H */
